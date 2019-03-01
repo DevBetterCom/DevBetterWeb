d4f1b7e8ac1407dae03d15a8098a8e018b7e3faa
Steve Smith <steve@kentsmiths.com>
2019-03-01 13:19:25 -0500

    Hiding Links from Non-Admin Users for Create/Edit/Delete (#18)
    * Member role required for List; admin for write access
    
    * Hiding Links from Non-Admin Users

--- src/DevBetterWeb.Web/Constants.cs
+++ src/DevBetterWeb.Web/Constants.cs
@@ -1,24 +1,13 @@
-﻿using CleanArchitecture.Core.Interfaces;
-using CleanArchitecture.Core.SharedKernel;
-using CleanArchitecture.Infrastructure.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using StructureMap;
-using Swashbuckle.AspNetCore.Swagger;
-using System;
-
-namespace CleanArchitecture.Web
+﻿namespace DevBetterWeb.Web
 {
     public static class Constants
     {
         public static class Roles
         {
             public const string ADMINISTRATORS = "Administrators";
+            public const string MEMBERS = "Members";
+
+            public const string ADMINISTRATORS_MEMBERS = "Administrators,Members";
         }
     }
 }
