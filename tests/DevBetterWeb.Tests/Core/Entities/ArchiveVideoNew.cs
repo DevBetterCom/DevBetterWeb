c5a489eca77e28a8170524db8fe18365b5502151
Steve Smith <steve@kentsmiths.com>
2019-03-08 13:06:26 -0500

    Adding a failing test (#22)
    * Adding a failing test
    
    * Fixing bug to make test pass
    
    * Adding coverlet nuget package

---
+++ tests/DevBetterWeb.Tests/Core/Entities/ArchiveVideoNew.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Core.Entities;
+using Xunit;
+
+namespace CleanArchitecture.Tests.Core.Entities
+{
+    public class ArchiveVideoNew
+    {
+        [Fact]
+        public void InitializesListOfQuestions()
+        {
+            var video = new ArchiveVideo();
+
+            Assert.NotNull(video.Questions);
+        }
+    }
+}
