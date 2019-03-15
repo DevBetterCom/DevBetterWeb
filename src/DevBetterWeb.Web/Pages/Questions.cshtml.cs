4d011986cc214599d5454d7059b04b88d0888944
Steve Smith <steve@kentsmiths.com>
2019-03-15 12:29:06 -0400

    Formatted questions page

--- src/DevBetterWeb.Web/Pages/Questions.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/Questions.cshtml.cs
@@ -19,11 +19,39 @@ namespace DevBetterWeb.Web.Pages
             _context = context;
         }
 
-        public IList<Question> Question { get;set; }
+        public IList<Question> Question { get; set; }
+        public IList<ArchiveVideo> Videos { get; set; }
 
         public async Task OnGetAsync()
         {
+            // seed data
+            if(_context.ArchiveVideos.Count() == 0)
+            {
+                var vid1 = new ArchiveVideo()
+                {
+                    Title = "Video One",
+                    DateCreated =new DateTime(2019, 3, 8)
+                };
+                var vid2 = new ArchiveVideo()
+                {
+                    Title = "Video Two",
+                    DateCreated = new DateTime(2019, 3, 15)
+                };
+                var questionA = new Question() { QuestionText = "How do I A?" };
+                var questionB = new Question() { QuestionText = "How do I B?" };
+                vid1.Questions.Add(questionA);
+                vid1.Questions.Add(questionB);
+
+                _context.ArchiveVideos.Add(vid1);
+                _context.ArchiveVideos.Add(vid2);
+                _context.SaveChanges();
+            }
+
             Question = await _context.Questions.ToListAsync();
+            Videos = await _context.ArchiveVideos
+                .Include(v => v.Questions)
+                .OrderByDescending(v => v.DateCreated)
+                .ToListAsync();
         }
     }
 }
