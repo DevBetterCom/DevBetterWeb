ff410f895a3b4fe65648037618e494cb3be1c2d6
Steve Smith <steve@kentsmiths.com>
2019-05-03 12:57:22 -0400

    Fixed Delete to not use entities

--- src/DevBetterWeb.Web/Pages/ArchivedVideos/Delete.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/ArchivedVideos/Delete.cshtml.cs
@@ -1,8 +1,12 @@
 ﻿using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace DevBetterWeb.Web.Pages.ArchivedVideos
@@ -10,15 +14,31 @@ namespace DevBetterWeb.Web.Pages.ArchivedVideos
     [Authorize(Roles = Constants.Roles.ADMINISTRATORS)]
     public class DeleteModel : PageModel
     {
-        private readonly CleanArchitecture.Infrastructure.Data.AppDbContext _context;
+        private readonly AppDbContext _context;
 
-        public DeleteModel(CleanArchitecture.Infrastructure.Data.AppDbContext context)
+        public DeleteModel(AppDbContext context)
         {
             _context = context;
         }
 
-        [BindProperty]
-        public ArchiveVideo ArchiveVideo { get; set; }
+        protected class DeleteVideoModel
+        {
+            public int Id { get; set; }
+        }
+
+        protected ArchiveVideoDeleteDTO ArchiveVideoToDelete { get; set; }
+
+        protected class ArchiveVideoDeleteDTO
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+
+            [DisplayName("Date Created")]
+            public DateTimeOffset DateCreated { get; set; }
+
+            [DisplayName("Video URL")]
+            public string VideoUrl { get; set; }
+        }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -27,12 +47,19 @@ namespace DevBetterWeb.Web.Pages.ArchivedVideos
                 return NotFound();
             }
 
-            ArchiveVideo = await _context.ArchiveVideos.FirstOrDefaultAsync(m => m.Id == id);
+            var archiveVideo = await _context.ArchiveVideos.FirstOrDefaultAsync(m => m.Id == id);
 
-            if (ArchiveVideo == null)
+            if (archiveVideo == null)
             {
                 return NotFound();
             }
+            ArchiveVideoToDelete = new ArchiveVideoDeleteDTO
+            {
+                Id = archiveVideo.Id,
+                Title = archiveVideo.Title,
+                DateCreated = archiveVideo.DateCreated,
+                VideoUrl = archiveVideo.VideoUrl
+            };
             return Page();
         }
 
@@ -43,11 +70,11 @@ namespace DevBetterWeb.Web.Pages.ArchivedVideos
                 return NotFound();
             }
 
-            ArchiveVideo = await _context.ArchiveVideos.FindAsync(id);
+            var archiveVideo = await _context.ArchiveVideos.FindAsync(id);
 
-            if (ArchiveVideo != null)
+            if (archiveVideo != null)
             {
-                _context.ArchiveVideos.Remove(ArchiveVideo);
+                _context.ArchiveVideos.Remove(archiveVideo);
                 await _context.SaveChangesAsync();
             }
 
