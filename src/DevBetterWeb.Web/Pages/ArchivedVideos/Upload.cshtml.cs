5b8bcc15c9dfd8065128a4dca314b39f71398e51
Steve Smith <steve@kentsmiths.com>
2019-05-10 12:43:30 -0400

    Adding azure file upload functionality

---
+++ src/DevBetterWeb.Web/Pages/ArchivedVideos/Upload.cshtml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace DevBetterWeb.Web.Pages.ArchivedVideos
+{
+    public class UploadModel : PageModel
+    {
+        private IConfiguration _configuration;
+
+        public UploadModel(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async void OnPost(List<IFormFile> files)
+        {
+            var uploadSuccess = false;
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length <= 0)
+                {
+                    continue;
+                }
+
+                // NOTE: uncomment either OPTION A or OPTION B to use one approach over another
+
+                // OPTION A: convert to byte array before upload
+                using (var ms = new MemoryStream())
+                {
+                    formFile.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+                    uploadSuccess = await UploadToBlob(formFile.FileName, fileBytes, null);
+
+                }
+
+                // OPTION B: read directly from stream for blob upload      
+                //using (var stream = formFile.OpenReadStream())
+                //{
+                //    uploadSuccess = await UploadToBlob(formFile.FileName, null, stream);
+                //}
+
+            }
+
+            if (uploadSuccess)
+            {
+                RedirectToPage("Index");
+            }
+
+        }
+
+        private async Task<bool> UploadToBlob(string filename, byte[] imageBuffer = null, Stream stream = null)
+        {
+            CloudStorageAccount storageAccount = null;
+            CloudBlobContainer cloudBlobContainer = null;
+            string storageConnectionString = _configuration["storageconnectionstring"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Create the CloudBlobClient that represents the Blob storage endpoint for the storage account.
+                    CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                    cloudBlobContainer = cloudBlobClient.GetContainerReference("videos");
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(filename);
+
+                    if (imageBuffer != null)
+                    {
+                        // OPTION A: use imageBuffer (converted from memory stream)
+                        await cloudBlockBlob.UploadFromByteArrayAsync(imageBuffer, 0, imageBuffer.Length);
+                    }
+                    else if (stream != null)
+                    {
+                        // OPTION B: pass in memory stream directly
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }
+                catch (StorageException ex)
+                {
+                    throw;
+
+                    return false;
+                }
+                finally
+                {
+                    // OPTIONAL: Clean up resources, e.g. blob container
+                    //if (cloudBlobContainer != null)
+                    //{
+                    //    await cloudBlobContainer.DeleteIfExistsAsync();
+                    //}
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
