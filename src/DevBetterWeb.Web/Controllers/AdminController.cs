d352211ec189949fd905e1fb68bb1f532967051d
Joe-LaRue <51251572+Joe-LaRue@users.noreply.github.com>
2019-07-11 20:33:42 -0400

    Admin section (#33)
    * way too much for one commit
    
    * added add/remove user from role functionality
    
    * added authorize attribute to admin controller.  added admin link to header.

---
+++ src/DevBetterWeb.Web/Controllers/AdminController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevBetterWeb.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using DevBetterWeb.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
+using DevBetterWeb.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DevBetterWeb.Web.Controllers
+{
+    [Authorize(Roles = Constants.Roles.ADMINISTRATORS)]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+           _roleManager = roleManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            var viewModel = new AdminIndexViewModel();
+            viewModel.Users = _userManager.Users.ToList();
+            viewModel.Roles = _roleManager.Roles.ToList();
+
+            return View(viewModel);
+        }
+
+        public IActionResult User(string userId)
+        {
+            var userViewModel = new UserViewModel();
+
+            userViewModel.User = _userManager.Users.FirstOrDefault(x => x.Id == userId);
+            return View(userViewModel);
+        }
+
+        public async Task<IActionResult> Role(string roleId)
+        {
+            var role = _roleManager.Roles.FirstOrDefault(x => x.Id == roleId);
+
+            var usersInRole = await _userManager.GetUsersInRoleAsync(role.Name);
+
+            var userIdsInRole = usersInRole.Select(X => X.Id).ToList();
+            var usersNotInRole = _userManager.Users.Where(x => !userIdsInRole.Contains(x.Id)).ToList();
+
+            var roleViewModel = new RoleViewModel();
+            roleViewModel.Role = role;
+            roleViewModel.UsersInRole = usersInRole.ToList();
+            roleViewModel.UsersNotInRole = usersNotInRole.Select(x => new SelectListItem(x.Email, x.Id)).ToList();
+
+
+            return View(roleViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUserToRole(string userId, string roleId)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userId);
+            var role = _roleManager.Roles.FirstOrDefault(x => x.Id == roleId);
+
+            if (user == null || role == null)
+            {
+                return BadRequest();
+            }
+
+            await _userManager.AddToRoleAsync(user, role.Name);
+
+            return RedirectToAction("role", new { roleId });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveUserFromRole(string userId, string roleId)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userId);
+            var role = _roleManager.Roles.FirstOrDefault(x => x.Id == roleId);
+
+            if (user == null || role == null)
+            {
+                return BadRequest();
+            }
+
+            await _userManager.RemoveFromRoleAsync(user, role.Name);
+
+            return RedirectToAction("role", new { roleId });
+        }
+    }
+}
---
+++ src/DevBetterWeb.Web/ViewModels/AdminIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevBetterWeb.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Web.ViewModels
+{
+    public class AdminIndexViewModel
+    {
+        public List<ApplicationUser> Users { get; set; }
+        public List<IdentityRole> Roles  { get; set; }
+
+    }
+}
