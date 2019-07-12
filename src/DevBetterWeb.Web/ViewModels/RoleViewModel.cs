802d7432953c9110f844073cbf612fa35ea8a71b
Joe-LaRue <51251572+Joe-LaRue@users.noreply.github.com>
2019-07-11 20:33:42 -0400

    Admin section (#33)
    * way too much for one commit
    
    * added add/remove user from role functionality
    
    * added authorize attribute to admin controller.  added admin link to header.

---
+++ src/DevBetterWeb.Web/ViewModels/RoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevBetterWeb.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Web.ViewModels
+{
+    public class RoleViewModel
+    {
+        public IdentityRole Role { get; set; }
+        public List<ApplicationUser> UsersInRole { get; set; }
+        public List<SelectListItem> UsersNotInRole { get; set; }
+    }
+}
---
+++ src/DevBetterWeb.Web/wwwroot/js/admin.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+
+    $('#removeUserModal').on('show.bs.modal', function (e) {
+        var button = $(e.relatedTarget);
+
+        var userEmail = button.attr('data-userEmail');
+        var userId = button.attr('data-userId');
+
+        $('#spnRemoveUserEmail').html(userEmail);
+        $('#hidRemoveUserId').val(userId);
+    });
+});
