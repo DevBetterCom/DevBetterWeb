a3a3aecb185ed6e504edc2d8f6ee94088a22022e
Joe LaRue <jlarue617@outlook.com>
2020-02-15 18:57:22 -0500

    craeted myprofile page for editing user data.

---
+++ src/DevBetterWeb.Web/Pages/User/MyProfile.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevBetterWeb.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevBetterWeb.Web.Pages.User
+{
+    public class MyProfileModel : PageModel
+    {
+        [BindProperty]
+        public UserProfileUpdateModel UserProfileUpdateModel { get; set; }
+
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public MyProfileModel(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+        }
+
+       
+
+        public async Task OnGet()
+        {
+            var currentUserName = User.Identity.Name;
+            var applicationUser = await _userManager.FindByNameAsync(currentUserName);
+
+            UserProfileUpdateModel = new UserProfileUpdateModel(applicationUser);
+        }
+
+        public async Task OnPost()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+            
+            var currentUserName = User.Identity.Name;
+            var applicationUser = await _userManager.FindByNameAsync(currentUserName);
+
+           
+
+            applicationUser.FirstName = UserProfileUpdateModel.FirstName;
+            applicationUser.LastName = UserProfileUpdateModel.LastName;
+            applicationUser.AboutInfo = UserProfileUpdateModel.AboutInfo;
+            applicationUser.Address = UserProfileUpdateModel.Address;
+            applicationUser.BlogUrl = UserProfileUpdateModel.BlogUrl;
+            applicationUser.GithubUrl = UserProfileUpdateModel.GithubUrl;
+            applicationUser.LinkedInUrl = UserProfileUpdateModel.LinkedInUrl;
+            applicationUser.TwitterUrl = UserProfileUpdateModel.TwitterUrl;
+            applicationUser.TwitchUrl = UserProfileUpdateModel.TwitchUrl;
+            applicationUser.OtherUrl = UserProfileUpdateModel.OtherUrl;
+
+            await _userManager.UpdateAsync(applicationUser);
+        }
+    }
+}
---
+++ src/DevBetterWeb.Web/Pages/User/UserProfileUpdateModel.cs
@@ -0,0 +1,44 @@
+﻿using DevBetterWeb.Web.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Web.Pages.User
+{
+    public class UserProfileUpdateModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string TwitterUrl { get; set; }
+        public string GithubUrl { get; set; }
+        public string BlogUrl { get; set; }
+        public string TwitchUrl { get; set; }
+        public string OtherUrl { get; set; }
+        public string AboutInfo { get; set; }
+
+        public UserProfileUpdateModel()
+        {
+
+        }
+
+        public UserProfileUpdateModel(ApplicationUser applicationUser)
+        {
+            BlogUrl = applicationUser.BlogUrl;
+            TwitchUrl = applicationUser.TwitchUrl;
+            TwitterUrl = applicationUser.TwitterUrl;
+            GithubUrl = applicationUser.GithubUrl;
+            LinkedInUrl = applicationUser.LinkedInUrl;
+            OtherUrl = applicationUser.OtherUrl;
+            AboutInfo = applicationUser.AboutInfo;
+            FirstName = applicationUser.FirstName;
+            LastName = applicationUser.LastName;
+            Address = applicationUser.Address;
+        }
+    }
+}
