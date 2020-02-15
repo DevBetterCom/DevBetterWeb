ab2b8a8e397d36afe8c97102c88e20beed1a09c7
Joe LaRue <jlarue617@outlook.com>
2020-02-15 07:01:18 -0500

    added properties to identity user and created a new migration

--- src/DevBetterWeb.Infrastructure/Identity/Data/ApplicationUser.cs
+++ src/DevBetterWeb.Infrastructure/Identity/Data/ApplicationUser.cs
@@ -5,5 +5,14 @@ namespace DevBetterWeb.Web.Areas.Identity.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Address { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string TwitterUrl { get; set; }
+        public string GithubUrl { get; set; }
+        public string BlogUrl { get; set; }
+        public string TwitchUrl { get; set; }
+        public string OtherUrl { get; set; }
+        public string AboutInfo { get; set; }
+
     }
 }
--- src/DevBetterWeb.Infrastructure/Identity/Data/IdentityDbContext.cs
+++ src/DevBetterWeb.Infrastructure/Identity/Data/IdentityDbContext.cs
@@ -17,6 +17,16 @@ namespace DevBetterWeb.Web.Models
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().Property(x => x.Address).HasMaxLength(500);
+            builder.Entity<ApplicationUser>().Property(x => x.BlogUrl).HasMaxLength(200);
+            builder.Entity<ApplicationUser>().Property(x => x.GithubUrl).HasMaxLength(200);
+            builder.Entity<ApplicationUser>().Property(x => x.LinkedInUrl).HasMaxLength(200);
+            builder.Entity<ApplicationUser>().Property(x => x.OtherUrl).HasMaxLength(200);
+            builder.Entity<ApplicationUser>().Property(x => x.TwitchUrl).HasMaxLength(200);
+            builder.Entity<ApplicationUser>().Property(x => x.TwitterUrl).HasMaxLength(200);
+
+
         }
     }
 }
