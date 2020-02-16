00792a088d61b867a12447f207db8a93e8d82511
Joe LaRue <jlarue617@outlook.com>
2020-02-15 19:03:24 -0500

    added security for my profile page and header link

--- src/DevBetterWeb.Web/Pages/User/MyProfile.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/User/MyProfile.cshtml.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DevBetterWeb.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DevBetterWeb.Web.Pages.User
 {
+
+    [Authorize]
     public class MyProfileModel : PageModel
     {
         [BindProperty]
