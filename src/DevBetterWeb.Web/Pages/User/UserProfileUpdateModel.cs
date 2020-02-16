4f9a94ff75fdc78259e7c50b7e7633bcde972028
Joe LaRue <jlarue617@outlook.com>
2020-02-15 20:08:56 -0500

    added URL Validation Attribute

--- src/DevBetterWeb.Web/Pages/User/UserProfileUpdateModel.cs
+++ src/DevBetterWeb.Web/Pages/User/UserProfileUpdateModel.cs
@@ -14,11 +14,17 @@ namespace DevBetterWeb.Web.Pages.User
         [Required]
         public string LastName { get; set; }
         public string Address { get; set; }
+        [ValidUrl]
         public string LinkedInUrl { get; set; }
+        [ValidUrl]
         public string TwitterUrl { get; set; }
+        [ValidUrl]
         public string GithubUrl { get; set; }
+        [ValidUrl]
         public string BlogUrl { get; set; }
+        [ValidUrl]
         public string TwitchUrl { get; set; }
+        [ValidUrl]
         public string OtherUrl { get; set; }
         public string AboutInfo { get; set; }
 
