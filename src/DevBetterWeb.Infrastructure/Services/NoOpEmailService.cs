5d107a41c2a9b7a04f7e777100ca4e0e44aa06d2
Steve Smith <steve@kentsmiths.com>
2020-03-16 17:05:37 -0400

    Remove no op email service

--- src/DevBetterWeb.Infrastructure/Services/NoOpEmailService.cs
+++ src/DevBetterWeb.Infrastructure/Services/NoOpEmailService.cs
@@ -7,11 +7,11 @@ namespace DevBetterWeb.Infrastructure.Services
     /// <summary>
     /// Used for local development
     /// </summary>
-    public class NoOpEmailService : IEmailService
-    {
-        public Task SendEmailAsync(string email, string subject, string message)
-        {
-            return Task.CompletedTask;
-        }
-    }
+    //public class NoOpEmailService : IEmailService
+    //{
+    //    public Task SendEmailAsync(string email, string subject, string message)
+    //    {
+    //        return Task.CompletedTask;
+    //    }
+    //}
 }
