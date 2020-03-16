53187cf37ce8252db3139341a4fcd2c2c78e2c24
Steve Smith <steve@kentsmiths.com>
2020-03-16 15:52:38 -0400

    Implementing forgot password

---
+++ src/DevBetterWeb.Infrastructure/Services/SendGridEmailSender.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Infrastructure.Services
+{
+    /// <summary>
+    /// Used by Forgot Password and similar Identity features
+    /// </summary>
+    public class SendGridEmailSender : IEmailSender
+    {
+        private readonly SendGridEmailService _emailService;
+
+        public SendGridEmailSender(SendGridEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+           await _emailService.SendEmailAsync(email, subject, htmlMessage);
+        }
+    }
+}
