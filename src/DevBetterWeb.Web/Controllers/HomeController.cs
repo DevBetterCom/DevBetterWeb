91799c94eb540e14ecad303c5296ad1ae7833cd9
Steve Smith <steve@kentsmiths.com>
2020-03-17 12:01:31 -0400

    adding env comment

--- src/DevBetterWeb.Web/Controllers/HomeController.cs
+++ src/DevBetterWeb.Web/Controllers/HomeController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevBetterWeb.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
         public IActionResult Index()
         {
+            ViewData.Add("env", _webHostEnvironment.EnvironmentName);
             return View();
         }
 
