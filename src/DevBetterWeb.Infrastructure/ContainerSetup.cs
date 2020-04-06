6f1784019b45959e41b88796224b4271bc08d15f
Steve Smith <steve@kentsmiths.com>
2020-04-06 14:54:15 -0400

    Cleaning up code; assing ListAllServices

--- src/DevBetterWeb.Infrastructure/ContainerSetup.cs
+++ src/DevBetterWeb.Infrastructure/ContainerSetup.cs
@@ -1,15 +1,9 @@
 ﻿using Autofac;
-using Autofac.Extensions.DependencyInjection;
 using DevBetterWeb.Core.Entities;
 using DevBetterWeb.Infrastructure.Data;
 using DevBetterWeb.Infrastructure.DomainEvents;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevBetterWeb.Infrastructure
 {
@@ -21,16 +15,10 @@ namespace DevBetterWeb.Infrastructure
             var infrastructureAssembly = Assembly.GetAssembly(typeof(EfRepository)) ?? throw new Exception("Assembly not found.");
             //var sharedKernelAssembly = Assembly.GetAssembly(typeof(IRepository));
 
-            // Add Guard.AgainstNull for core and infra assemblies
-
             builder.RegisterAssemblyTypes(webAssembly, coreAssembly, infrastructureAssembly).AsImplementedInterfaces();
 
             // register specific types
             builder.RegisterType<DomainEventDispatcher>().InstancePerLifetimeScope();
-
-            //setupAction?.Invoke(builder);
-            //builder.Build();
-
         }
     }
 }
