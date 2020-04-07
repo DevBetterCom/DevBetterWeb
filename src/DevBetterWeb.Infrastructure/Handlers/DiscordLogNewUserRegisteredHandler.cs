e48505a969c733a92c24ad325db5319a610ca612
Steve Smith <steve@kentsmiths.com>
2020-04-07 16:49:33 -0400

    Adding discord handler for new user registrations

---
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogNewUserRegisteredHandler.cs
@@ -0,0 +1,23 @@
+﻿using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.Services;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Core.Handlers
+{
+    public class DiscordLogNewUserRegisteredHandler : IHandle<NewUserRegisteredEvent>
+    {
+        private readonly Webhook _webhook;
+
+        public DiscordLogNewUserRegisteredHandler(Webhook webhook)
+        {
+            _webhook = webhook;
+        }
+
+        public async Task Handle(NewUserRegisteredEvent domainEvent)
+        {
+            _webhook.Content = $"New user registered with email address: {domainEvent.EmailAddress}.";
+            await _webhook.Send();
+        }
+    }
+}
