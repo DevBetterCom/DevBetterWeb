97ce61c871ce95de125c422469894731bc30b4c0
Steve Smith <steve@kentsmiths.com>
2020-04-07 12:02:36 -0400

    Adding event when uncomfirmed user tries to reset pwd

---
+++ src/DevBetterWeb.Core/Events/InvalidUserEvent.cs
@@ -0,0 +1,14 @@
+﻿using DevBetterWeb.Core.SharedKernel;
+
+namespace DevBetterWeb.Core.Events
+{
+    public class InvalidUserEvent : BaseDomainEvent
+    {
+        public InvalidUserEvent(string emailAddress)
+        {
+            EmailAddress = emailAddress;
+        }
+
+        public string EmailAddress { get; }
+    }
+}
