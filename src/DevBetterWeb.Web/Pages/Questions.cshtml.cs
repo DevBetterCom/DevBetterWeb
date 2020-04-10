48f5a07b2e52f69dfd79cb8dab0b7c6aa4e4dd32
Steve Smith <steve@kentsmiths.com>
2020-04-10 12:37:09 -0400

    Adding Questions Page and test back in

---
+++ src/DevBetterWeb.Web/Pages/Questions.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBetterWeb.Web.Pages
+{
+    public class QuestionsModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public QuestionsModel(AppDbContext context)
+        {
+            Guard.Against.Null(context, nameof(context));
+            _context = context;
+        }
+
+        public IList<Question> Question { get; set; } = new List<Question>();
+        public IList<ArchiveVideo> Videos { get; set; } = new List<ArchiveVideo>();
+
+        public async Task OnGetAsync()
+        {
+            Question = await _context.Questions.ToListAsync();
+            Videos = await _context.ArchiveVideos
+                .Include(v => v.Questions)
+                .OrderByDescending(v => v.DateCreated)
+                .ToListAsync();
+        }
+    }
+}
