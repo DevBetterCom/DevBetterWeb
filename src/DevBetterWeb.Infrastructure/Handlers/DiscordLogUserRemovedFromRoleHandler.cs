8d1da061102da63e7bb66b77a43610081812171c
Steve Smith <steve@kentsmiths.com>
2020-04-10 13:03:23 -0400

    Adding events and notifications on role changes

---
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogUserRemovedFromRoleHandler.cs
@@ -0,0 +1,23 @@
+﻿using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.Services;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Core.Handlers
+{
+    public class DiscordLogUserRemovedFromRoleHandler : IHandle<UserRemovedFromRoleEvent>
+    {
+        private readonly Webhook _webhook;
+
+        public DiscordLogUserRemovedFromRoleHandler(Webhook webhook)
+        {
+            _webhook = webhook;
+        }
+
+        public async Task Handle(UserRemovedFromRoleEvent domainEvent)
+        {
+            _webhook.Content = $"User {domainEvent.EmailAddress} removed from role {domainEvent.Role}.";
+            await _webhook.Send();
+        }
+    }
+}
