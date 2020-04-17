b50d64eba9ebeddcfc6e6b36a0457ab7e0a434d1
Steve Smith <steve@kentsmiths.com>
2020-04-17 12:49:07 -0400

    Added registration for IEmailSender used by Identity

--- src/DevBetterWeb.Infrastructure/DefaultInfrastructureModule.cs
+++ src/DevBetterWeb.Infrastructure/DefaultInfrastructureModule.cs
@@ -3,6 +3,7 @@ using DevBetterWeb.Core.Interfaces;
 using DevBetterWeb.Core.Services;
 using DevBetterWeb.Infrastructure.DomainEvents;
 using DevBetterWeb.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace DevBetterWeb.Infrastructure
 {
@@ -34,6 +35,7 @@ namespace DevBetterWeb.Infrastructure
             builder.RegisterType<Webhook>().InstancePerDependency();
             builder.RegisterType<DomainEventDispatcher>().As<IDomainEventDispatcher>();
             builder.RegisterType<MemberRegistrationService>().As<IMemberRegistrationService>();
+            builder.RegisterType<DefaultEmailSender>().As<IEmailSender>();
         }
 
         private void RegisterDevelopmentOnlyDependencies(ContainerBuilder builder)
