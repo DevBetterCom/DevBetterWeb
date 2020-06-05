16fe6b61622a05c60b9901934379c070e0d1f7f9
Steve Smith <steve@kentsmiths.com>
2020-06-05 08:53:05 -0400

    Wiring up errors to send discord events

---
+++ src/DevBetterWeb.Core/Events/SiteErrorOccurredEvent.cs
@@ -0,0 +1,15 @@
+﻿using DevBetterWeb.Core.SharedKernel;
+using System;
+
+namespace DevBetterWeb.Core.Events
+{
+    public class SiteErrorOccurredEvent : BaseDomainEvent
+    {
+        public SiteErrorOccurredEvent(Exception siteException)
+        {
+            SiteException = siteException;
+        }
+
+        public Exception SiteException { get; }
+    }
+}
--- src/DevBetterWeb.Web/Controllers/HomeController.cs
+++ src/DevBetterWeb.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevBetterWeb.Web.Controllers
@@ -6,11 +9,18 @@ namespace DevBetterWeb.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IExceptionHandlerPathFeature _exceptionHandlerPathFeature;
+        private readonly IDomainEventDispatcher _dispatcher;
 
-        public HomeController(IWebHostEnvironment webHostEnvironment)
+        public HomeController(IWebHostEnvironment webHostEnvironment,
+            IExceptionHandlerPathFeature exceptionHandlerPathFeature,
+            IDomainEventDispatcher dispatcher)
         {
             _webHostEnvironment = webHostEnvironment;
+            _exceptionHandlerPathFeature = exceptionHandlerPathFeature;
+            _dispatcher = dispatcher;
         }
+
         public IActionResult Index()
         {
             ViewData.Add("env", _webHostEnvironment.EnvironmentName);
@@ -19,6 +29,11 @@ namespace DevBetterWeb.Web.Controllers
 
         public IActionResult Error()
         {
+            if(_exceptionHandlerPathFeature != null)
+            {
+                var exceptionEvent = new SiteErrorOccurredEvent(_exceptionHandlerPathFeature.Error);
+                _dispatcher.Dispatch(exceptionEvent);
+            }
             return View();
         }
     }
