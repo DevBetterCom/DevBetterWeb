4c19fa79175aad4930657adaf500c04153107b74
Steve Smith <steve@kentsmiths.com>
2020-06-05 08:53:05 -0400

    Wiring up errors to send discord events

---
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogSiteErrorOccurredHandler.cs
@@ -0,0 +1,23 @@
+﻿using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.Services;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Core.Handlers
+{
+    public class DiscordLogSiteErrorOccurredHandler : IHandle<SiteErrorOccurredEvent>
+    {
+        private readonly Webhook _webhook;
+
+        public DiscordLogSiteErrorOccurredHandler(Webhook webhook)
+        {
+            _webhook = webhook;
+        }
+
+        public async Task Handle(SiteErrorOccurredEvent domainEvent)
+        {
+            _webhook.Content = $"Site error: {domainEvent.SiteException.ToString()}.";
+            await _webhook.Send();
+        }
+    }
+}
