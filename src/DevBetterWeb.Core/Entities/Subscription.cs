b8a49f47e210d225a94663c45e00bd951e1bbfd8
shmuel <samuelwine@gmail.com>
2020-07-22 00:31:21 +0100

    Create Subscription entity with properties

---
+++ src/DevBetterWeb.Core/Entities/Subscription.cs
@@ -0,0 +1,12 @@
+﻿using DevBetterWeb.Core.SharedKernel;
+using System;
+
+namespace DevBetterWeb.Core.Entities
+{
+    class Subscription : BaseEntity
+    {
+        public int MemberId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
