6f0b22ddd6104ef7d22ac498714c672d1d8b29a4
shmuel <samuelwine@gmail.com>
2020-07-28 00:34:41 +0100

    Add functionality to retrieve subscriptions Add subscription DTO map subscription entity to DTO

--- src/DevBetterWeb.Web/Pages/Admin/User.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/Admin/User.cshtml.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DevBetterWeb.Core;
-using DevBetterWeb.Core.Events;
+﻿using DevBetterWeb.Core;
 using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.Data;
 using DevBetterWeb.Web.Areas.Identity.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -11,6 +8,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DevBetterWeb.Web.Pages.Admin
 {
@@ -20,20 +21,23 @@ namespace DevBetterWeb.Web.Pages.Admin
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IUserRoleMembershipService _userRoleMembershipService;
+        private readonly AppDbContext _context;
 
         public UserModel(UserManager<ApplicationUser> userManager, 
             RoleManager<IdentityRole> roleManager,
-            IUserRoleMembershipService userRoleMembershipService)
+            IUserRoleMembershipService userRoleMembershipService,
+            AppDbContext context)
         {
             _userManager = userManager;
             _roleManager = roleManager;
             _userRoleMembershipService = userRoleMembershipService;
+            _context = context;
         }
 
         public IdentityUser? IdentityUser { get; set; }
-
         public List<IdentityRole> Roles { get; set; } = new List<IdentityRole>();
         public List<SelectListItem> RolesNotAssignedToUser { get; set; } = new List<SelectListItem>();
+        public List<SubscriptionDTO> Subscriptions { get; set; } = new List<SubscriptionDTO>();
 
 
         public async Task<IActionResult> OnGetAsync(string userId)
@@ -70,6 +74,21 @@ namespace DevBetterWeb.Web.Pages.Admin
             RolesNotAssignedToUser = unassignedRoles.Select(x => new SelectListItem(x.Name, x.Id)).ToList();
             Roles = assignedRoles.ToList();
 
+            var subscriptions = await _context.Subscriptions
+                                                .Where(x => x.Member.UserId == userId)
+                                                .OrderBy(x => x.StartDate)
+                                                .ToListAsync();
+            
+            foreach (var subscription in subscriptions)
+            {
+                Subscriptions.Add(new SubscriptionDTO()
+                {
+                    Id = subscription.Id,
+                    StartDate = subscription.StartDate,
+                    EndDate = subscription.EndDate
+                });
+            }
+
             return Page();
         }
 
@@ -85,6 +104,13 @@ namespace DevBetterWeb.Web.Pages.Admin
             await _userRoleMembershipService.RemoveUserFromRoleAsync(userId, roleId);
 
             return RedirectToPage("./User", new { userId = userId});
+        }
+
+        public class SubscriptionDTO
+        {
+            public int Id { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
         }
     }
 }
