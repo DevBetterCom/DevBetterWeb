a41ce1e7948dc66130c00278d225a5a1fbd6a4ed
ilyanaDev <42644084+ilyanaDev@users.noreply.github.com>
2020-08-25 08:08:34 -0400

    Member update profile discord event (#124)
    * discord log user update handler should work to provide discord notification when a member updates their profile
    
    * discord should alert when member profile is updated; unit test added
    
    * log user update handler unit test fixed, small error fix on discord log user update handler

---
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogUserUpdateHandler.cs
@@ -0,0 +1,29 @@
+﻿using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.Services;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Core.Handlers
+{
+    public class DiscordLogUserUpdateHandler : IHandle<MemberUpdatedEvent>
+    {
+        private readonly Webhook _webhook;
+
+        public DiscordLogUserUpdateHandler(Webhook webhook)
+        {
+            _webhook = webhook;
+        }
+
+        public async Task Handle(MemberUpdatedEvent memberUpdatedEvent)
+        {
+            _webhook.Content = returnWebhookMessageString(memberUpdatedEvent);
+            await _webhook.Send();
+        }
+
+        public static string returnWebhookMessageString(MemberUpdatedEvent memberUpdatedEvent)
+        {
+            return $"User {memberUpdatedEvent.Member.FirstName} {memberUpdatedEvent.Member.LastName} just updated their profile. " +
+                $"Check it out here: https://devbetter.com/User/Details/{memberUpdatedEvent.Member.UserId}.";
+        }
+    }
+}
