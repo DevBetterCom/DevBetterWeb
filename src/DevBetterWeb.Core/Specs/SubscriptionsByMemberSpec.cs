e57ad990861d85539a1a5e01678b8842d909bd83
shmuel <samuelwine@gmail.com>
2020-08-26 21:27:08 +0100

    Add specification for retrieving subcriptions by member

---
+++ src/DevBetterWeb.Core/Specs/SubscriptionsByMemberSpec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs
+{
+    public class SubscriptionsByMemberSpec : BaseSpecification<Subscription>
+    {
+        public SubscriptionsByMemberSpec(string userId)
+        {
+            AddCriteria(subscription => subscription.MemberId.ToString() == userId);
+            ApplyOrderBy(subscription => subscription.StartDate);
+        }
+    }
+}
+
