7b27e0593b3684734203e7cbd15a64d616cf197b
Ilyana Smith <ilyana@kentsmiths.com>
2020-09-24 13:46:16 -0400

    unit tests added for AddBookRead method of Member

---
+++ tests/DevBetterWeb.Tests/Core/Entities/MemberTests/MemberAddBook.cs
@@ -0,0 +1,45 @@
+﻿using DevBetterWeb.Core.Entities;
+using Xunit;
+
+namespace DevBetterWeb.Tests.Core.Entities.MemberTests
+{
+    public class MemberAddBook
+    {
+
+        private Book GetBookToAdd()
+        {
+            return new Book
+            {
+                Title = "Book",
+                Author = "Ilyana Smith",
+                Details = "this is a book",
+                PurchaseUrl = "https://buyabook.com"
+            };
+        }
+
+        [Fact]
+        public void ShouldAddBookGivenBook()
+        {
+            Member member = MemberHelpers.CreateWithDefaultConstructor();
+            Book book = GetBookToAdd();
+
+            member.AddBookRead(book);
+
+            Assert.Contains(book, member.BooksRead);
+        }
+
+        [Fact]
+        public void ShouldDoNothingGivenDuplicateBook()
+        {
+            Member member = MemberHelpers.CreateWithDefaultConstructor();
+            Book book = GetBookToAdd();
+
+            member.AddBookRead(book);
+            member.AddBookRead(book);
+
+            // if we get to this point no error was thrown.
+            Assert.Contains(book, member.BooksRead);
+        }
+
+    }
+}
