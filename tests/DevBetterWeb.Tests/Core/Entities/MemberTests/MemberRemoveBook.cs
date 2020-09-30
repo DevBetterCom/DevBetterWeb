fc06b2d1dfa428094ca83c44c37d79af59d031b3
Ilyana Smith <ilyana@kentsmiths.com>
2020-09-30 16:54:41 -0400

    tests added for member's RemoveBookRead method

--- tests/DevBetterWeb.Tests/Core/Entities/MemberTests/MemberRemoveBook.cs
+++ tests/DevBetterWeb.Tests/Core/Entities/MemberTests/MemberRemoveBook.cs
@@ -3,12 +3,22 @@ using Xunit;
 
 namespace DevBetterWeb.Tests.Core.Entities.MemberTests
 {
-
-    public class MemberAddBook
+    public class MemberRemoveBook
     {
-
         [Fact]
-        public void ShouldAddBookGivenBook()
+        public void ShouldDoNothingGivenBookNotInBooksRead()
+        {
+            Member member = MemberHelpers.CreateWithDefaultConstructor();
+            Book book = BookHelpers.GetDefaultBook();
+
+            member.RemoveBookRead(book);
+
+            // if we get this far, no error was thrown
+            Assert.Empty(member.BooksRead);
+        }
+
+        [Fact] 
+        public void ShouldRemoveBookGivenBook()
         {
             Member member = MemberHelpers.CreateWithDefaultConstructor();
             Book book = BookHelpers.GetDefaultBook();
@@ -16,21 +26,10 @@ namespace DevBetterWeb.Tests.Core.Entities.MemberTests
             member.AddBookRead(book);
 
             Assert.Contains(book, member.BooksRead);
+
+            member.RemoveBookRead(book);
+
+            Assert.Empty(member.BooksRead);
         }
-
-        [Fact]
-        public void ShouldDoNothingGivenDuplicateBook()
-        {
-            Member member = MemberHelpers.CreateWithDefaultConstructor();
-            Book book = BookHelpers.GetDefaultBook();
-
-            member.AddBookRead(book);
-            member.AddBookRead(book);
-
-            // if we get to this point no error was thrown.
-            Assert.Contains(book, member.BooksRead);
-            Assert.Single(member.BooksRead!);
-        }
-
     }
 }
