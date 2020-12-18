77bb4920d493236bb9478135de53cfa4c99831ef
Steve Smith <steve@kentsmiths.com>
2020-12-18 14:07:28 -0500

    Refactor bookadmin (#158)
    * Cleaned up some things
    Fire newbook added event when adding a new book
    
    * Refactored Book Create to use a DTO and Repository.

---
+++ src/DevBetterWeb.Web/Pages/Admin/Books/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DevBetterWeb.Web.Pages.Admin.Books
+{
+  public class BookViewModel
+  {
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Details { get; set; }
+    public string? PurchaseUrl { get; set; }
+  }
+}
