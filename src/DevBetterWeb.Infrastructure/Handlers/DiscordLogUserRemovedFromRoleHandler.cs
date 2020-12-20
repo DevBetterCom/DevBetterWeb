df9282626d733b5c07f6c59423df320e3c959fc6
Steve Smith <steve@kentsmiths.com>
2020-12-20 18:11:11 -0500

    Ardalis/profile photo (#165)
    * Basic avatar upload works - need to resize image.
    
    * Got basic avatar functionality working

--- src/DevBetterWeb.Infrastructure/Handlers/DiscordLogUserRemovedFromRoleHandler.cs
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogUserRemovedFromRoleHandler.cs
@@ -1,23 +1,23 @@
-﻿using DevBetterWeb.Core.Events;
+﻿using System.Threading.Tasks;
+using DevBetterWeb.Core.Events;
 using DevBetterWeb.Core.Interfaces;
 using DevBetterWeb.Infrastructure.Services;
-using System.Threading.Tasks;
 
 namespace DevBetterWeb.Core.Handlers
 {
-    public class DiscordLogUserRemovedFromRoleHandler : IHandle<UserRemovedFromRoleEvent>
+  public class DiscordLogUserRemovedFromRoleHandler : IHandle<UserRemovedFromRoleEvent>
+  {
+    private readonly AdminUpdatesWebhook _webhook;
+
+    public DiscordLogUserRemovedFromRoleHandler(AdminUpdatesWebhook webhook)
     {
-        private readonly AdminUpdatesWebhook _webhook;
-
-        public DiscordLogUserRemovedFromRoleHandler(AdminUpdatesWebhook webhook)
-        {
-            _webhook = webhook;
-        }
-
-        public async Task Handle(UserRemovedFromRoleEvent domainEvent)
-        {
-            _webhook.Content = $"User {domainEvent.EmailAddress} removed from role {domainEvent.Role}.";
-            await _webhook.Send();
-        }
+      _webhook = webhook;
     }
+
+    public Task Handle(UserRemovedFromRoleEvent domainEvent)
+    {
+      _webhook.Content = $"User {domainEvent.EmailAddress} removed from role {domainEvent.Role}.";
+      return _webhook.Send();
+    }
+  }
 }
