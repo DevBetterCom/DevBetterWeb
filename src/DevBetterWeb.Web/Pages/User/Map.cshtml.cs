588bcc2d79f61b2465925d143c38490eb0321698
shmuel <samuelwine@gmail.com>
2020-12-21 00:28:32 +0000

    Create Map Page and add demo map

---
+++ src/DevBetterWeb.Web/Pages/User/Map.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevBetterWeb.Web.Pages.User
+{
+    public class MapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
