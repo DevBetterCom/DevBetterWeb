f61491488c8edc4a3f553312d58d0c6ad7a1042d
Steve Smith <steve@kentsmiths.com>
2020-12-20 22:58:32 -0500

    Restrict leaderboard to folks with at least 1 book Add avatar to main member list

---
+++ src/DevBetterWeb.Core/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace DevBetterWeb.Core
+{
+  public class Constants
+  {
+    public const string AvatarImageUrlFormatString = "https://devbetter.blob.core.windows.net/photos/{0}.jpg";
+  }
+}
