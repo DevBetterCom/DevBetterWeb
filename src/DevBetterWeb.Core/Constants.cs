62c8e1653bd6404a24c533d084c8bc77ed56a12c
Steve Smith <steve@kentsmiths.com>
2020-12-21 11:36:33 -0500

    Refactoring constants

--- src/DevBetterWeb.Core/Constants.cs
+++ src/DevBetterWeb.Core/Constants.cs
@@ -1,7 +1,15 @@
 ﻿namespace DevBetterWeb.Core
 {
-  public class Constants
+  public static class Constants
   {
-    public const string AvatarImageUrlFormatString = "https://devbetter.blob.core.windows.net/photos/{0}.jpg";
+    public const string AVATAR_IMGURL_FORMAT_STRING = "https://devbetter.blob.core.windows.net/photos/{0}.jpg";
+    public const int MAX_UPLOAD_FILE_SIZE = 1_512_428_800; // 1500 MB
+    public const string FILE_DATE_FORMAT_STRING = "yyyy-MM-dd";
+
+
+    public static class ConfigKeys
+    {
+      public const string FileStorageConnectionString = "storageconnectionstring";
+    }
   }
 }
