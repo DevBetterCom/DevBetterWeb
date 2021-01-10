577a8a701555283d95815081d128177b4f4873f3
ShadyNagy <info@shadynagy.com>
2021-01-10 23:38:10 +0200

    Start with Vimeo.

---
+++ tests/DevBetterWeb.Tests/Services/VimeoServiceTests/GetFullAccessUrl.cs
@@ -0,0 +1,21 @@
+﻿using DevBetterWeb.Core.Entities;
+using System;
+using System.Threading.Tasks;
+using DevBetterWeb.Infrastructure.Services;
+using Xunit;
+
+namespace DevBetterWeb.Tests.Services.VimeoServiceTests
+{
+  public class GetFullAccessUrl
+  {
+    [Fact]
+    public async Task GetFullAccessUrlIsExpected()
+    {
+      var vimeoCredential = new VimeoCredential("client id", "client secret", "https://your_website_here.com/wherever-you-send-users-after-grant");
+      var vimeoService = new VimeoService(vimeoCredential);
+      var fullAccessToken = await vimeoService.GetFullAccessTokenAsync();
+
+      Assert.True(true);
+    }
+  }
+}
---
+++ tests/DevBetterWeb.Tests/Services/VimeoServiceTests/GetUnauthenticatedToken.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using DevBetterWeb.Infrastructure.Services;
+using Xunit;
+
+namespace DevBetterWeb.Tests.Services.VimeoServiceTests
+{
+  public class GetUnauthenticatedToken
+  {
+    [Fact]
+    public async Task GetUnauthenticatedTokenIsExpected()
+    {
+      var vimeoCredential = new VimeoCredential("client id", "client secret", "https://your_website_here.com/wherever-you-send-users-after-grant");
+      var vimeoService = new VimeoService(vimeoCredential);
+      var readAccessToken = await vimeoService.GetUnauthenticatedToken();
+
+      Assert.NotNull(readAccessToken);
+    }
+  }
+}
