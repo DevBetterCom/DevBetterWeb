80731c9cebc48e67bec24cef4c5ff1832d6a5c14
Shady Nagy <shadynagi@gmail.com>
2021-02-05 20:56:30 +0200

    Start with Vimeo. (#176)
    * Start with Vimeo.
    
    * unit test fix.
    
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

---
+++ src/DevBetterWeb.Infrastructure/Services/VimeoCredential.cs
@@ -0,0 +1,18 @@
+﻿namespace DevBetterWeb.Infrastructure.Services
+{
+  public class VimeoCredential
+  {
+    public string ClientId { get; }
+    public string ClientSecret { get; }
+    public string RedirectionUrl { get; }
+    public string? StateInformation { get; }
+
+    public VimeoCredential(string clientId, string clientSecret, string redirectionUrl, string? stateInformation=null)
+    {
+      ClientId = clientId;
+      ClientSecret = clientSecret;
+      RedirectionUrl = redirectionUrl;
+      StateInformation = stateInformation;
+    }
+  }
+}
