6320d226d77a2531b022141f945b08fd9c526f24
Rick Hodder <rickhodder@msn.com>
2021-03-28 12:54:51 -0400

    Created IAppLogger and LoggerAdapter (#212)

---
+++ src/DevBetterWeb.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,24 @@
+using DevBetterWeb.Core.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace DevBetterWeb.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+    }
+}
