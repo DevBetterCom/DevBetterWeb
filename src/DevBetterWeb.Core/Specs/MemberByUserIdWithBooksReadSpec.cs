2b2656d85d001d24755c6b78d9575d1b2e886651
Ilyana Smith <ilyana@kentsmiths.com>
2021-04-14 16:47:59 -0400

    works

--- src/DevBetterWeb.Core/Specs/MemberByUserIdWithBooksReadSpec.cs
+++ src/DevBetterWeb.Core/Specs/MemberByUserIdWithBooksReadSpec.cs
@@ -4,11 +4,11 @@ using DevBetterWeb.Core.Entities;
 namespace DevBetterWeb.Core.Specs
 {
   public class MemberByUserIdWithBooksReadSpec : Specification<Member>
+  {
+    public MemberByUserIdWithBooksReadSpec(string userId)
     {
-        public MemberByUserIdWithBooksReadSpec(string userId)
-        {
-            Query.Where(member => member.UserId == userId);
-            Query.Include(member => member.BooksRead);
-        }
+      Query.Where(member => member.UserId == userId)
+        .Include(member => member.BooksRead);
     }
+  }
 }
