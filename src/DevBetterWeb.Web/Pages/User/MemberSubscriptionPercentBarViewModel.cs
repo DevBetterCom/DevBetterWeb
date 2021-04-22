5eee686b81a781d080b15c0923e8e9797aaf7b7f
Ilyana Smith <ilyana@kentsmiths.com>
2021-04-22 09:24:26 -0400

    Fix member alumni progress percent circle

---
+++ src/DevBetterWeb.Web/Pages/User/MemberSubscriptionPercentBarViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace DevBetterWeb.Web.Pages.User
+{
+  public class MemberSubscriptionPercentBarViewModel
+  {
+    public int Percentage { get; set; }
+
+    public MemberSubscriptionPercentBarViewModel(int percentage)
+    {
+      Percentage = percentage;
+    }
+
+    public string GetPercentageWithPercent()
+    {
+      var percentageWithPercent = $"{Percentage}%";
+      return percentageWithPercent;
+    }
+  }
+
+  public class MemberSubscriptionPercentCircleViewModel
+  {
+    public int Percentage { get; set; }
+
+    public MemberSubscriptionPercentCircleViewModel(int percentage)
+    {
+      Percentage = percentage;
+    }
+
+    public string GetPercentageWithPercent()
+    {
+      var percentageWithPercent = $"{Percentage}%";
+      return percentageWithPercent;
+    }
+
+    public string GetDegrees()
+    {
+      var degrees = (int)(Percentage * ((double)360 / 100));
+      var degreesString = $"{degrees}deg";
+      return degreesString;
+    }
+
+  }
+}
