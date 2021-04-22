b6615270c259b0f3a18128bcb5d114354ae4f406
Ilyana Smith <ilyana@kentsmiths.com>
2021-04-22 15:16:01 -0400

    Subscription Percent Circle should finally work as intended

--- src/DevBetterWeb.Web/Pages/User/MemberSubscriptionPercentCircleViewModel.cs
+++ src/DevBetterWeb.Web/Pages/User/MemberSubscriptionPercentCircleViewModel.cs
@@ -15,11 +15,42 @@
       return percentageWithPercent;
     }
 
-    public string GetDegrees()
+    public int GetDegrees()
     {
       var degrees = (int)(Percentage * ((double)360 / 100));
+
+      return degrees;
+    }
+
+    public string GetDegreesForFirstPartialPie()
+    { 
+      if (Percentage <= 50)
+      {
+        var degreesStringUnder50 = $"{GetDegrees()}deg";
+        return degreesStringUnder50;
+      }
+      var degrees = GetDegrees() - 180;
       var degreesString = $"{degrees}deg";
       return degreesString;
+    }
+
+    public string GetDegreesForSecondPartialPie()
+    {
+      if(Percentage <= 50)
+      {
+        return "0deg";
+      }
+      var degreesString = $"{GetDegrees()}deg";
+      return degreesString;
+    }
+
+    public string GetOpacityForSecondPartialPie()
+    {
+      if(Percentage <= 50)
+      {
+        return "0";
+      }
+      return "1";
     }
 
   }
