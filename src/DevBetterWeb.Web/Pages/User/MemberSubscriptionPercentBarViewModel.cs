b05b0fed278cf68bc1e9e3d299c5ae85d716fc73
Ilyana Smith <ilyana@kentsmiths.com>
2021-04-22 09:26:22 -0400

    move member subscription percent circle view model to own file

--- src/DevBetterWeb.Web/Pages/User/MemberSubscriptionPercentBarViewModel.cs
+++ src/DevBetterWeb.Web/Pages/User/MemberSubscriptionPercentBarViewModel.cs
@@ -15,28 +15,4 @@
       return percentageWithPercent;
     }
   }
-
-  public class MemberSubscriptionPercentCircleViewModel
-  {
-    public int Percentage { get; set; }
-
-    public MemberSubscriptionPercentCircleViewModel(int percentage)
-    {
-      Percentage = percentage;
-    }
-
-    public string GetPercentageWithPercent()
-    {
-      var percentageWithPercent = $"{Percentage}%";
-      return percentageWithPercent;
-    }
-
-    public string GetDegrees()
-    {
-      var degrees = (int)(Percentage * ((double)360 / 100));
-      var degreesString = $"{degrees}deg";
-      return degreesString;
-    }
-
-  }
 }
---
+++ src/DevBetterWeb.Web/Pages/User/MemberSubscriptionPercentCircleViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace DevBetterWeb.Web.Pages.User
+{
+  public class MemberSubscriptionPercentCircleViewModel
+  {
+    public int Percentage { get; set; }
+
+    public MemberSubscriptionPercentCircleViewModel(int percentage)
+    {
+      Percentage = percentage;
+    }
+
+    public string GetPercentageWithPercent()
+    {
+      var percentageWithPercent = $"{Percentage}%";
+      return percentageWithPercent;
+    }
+
+    public string GetDegrees()
+    {
+      var degrees = (int)(Percentage * ((double)360 / 100));
+      var degreesString = $"{degrees}deg";
+      return degreesString;
+    }
+
+  }
+}
