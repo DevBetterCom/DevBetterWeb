ee41d5d5251d890273a25b5c7baf034f92d1b819
patrykr5 <rajca.patryk@gmail.com>
2021-05-16 17:29:45 +0200

    Added change email confirmation by admin and discord log for that

---
+++ src/DevBetterWeb.Core/Events/UserEmailConfirmedChangedEvent.cs
@@ -0,0 +1,16 @@
+﻿using DevBetterWeb.Core.SharedKernel;
+
+namespace DevBetterWeb.Core.Events
+{
+  public class UserEmailConfirmedChangedEvent : BaseDomainEvent
+  {
+    public UserEmailConfirmedChangedEvent(string emailAddress, bool isEmailConfirmed)
+    {
+      EmailAddress = emailAddress;
+      IsEmailConfirmed = isEmailConfirmed;
+    }
+
+    public string EmailAddress { get; }
+    public bool IsEmailConfirmed { get; }
+  }
+}
---
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogUserEmailConfirmedChangedHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.Services;
+
+namespace DevBetterWeb.Infrastructure.Handlers
+{
+  public class DiscordLogUserEmailConfirmedChangedHandler : IHandle<UserEmailConfirmedChangedEvent>
+  {
+    private readonly AdminUpdatesWebhook _webhook;
+
+    public DiscordLogUserEmailConfirmedChangedHandler(AdminUpdatesWebhook webhook)
+    {
+      _webhook = webhook;
+    }
+
+    public Task Handle(UserEmailConfirmedChangedEvent domainEvent)
+    {
+      _webhook.Content = $"For the user {domainEvent.EmailAddress} EmailConfirmed value has been changed to: {domainEvent.IsEmailConfirmed}";
+      return _webhook.Send();
+    }
+  }
+}
