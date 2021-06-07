44cbabeac68adea6ced092aee2a3b0b5e5776a04
ilyanaDev <42644084+ilyanaDev@users.noreply.github.com>
2021-06-07 14:16:19 -0400

    Admin cancel subscription flow (#315)
    * admin subscription cancellation functionality is nearly complete. But for some reason _userManager.GetEmail Async (line 41 of Admin/Members/Cancel.cshtml.cs) is not yet working as intended
    
    * admin cancellation works

---
+++ src/DevBetterWeb.Web/Pages/Admin/Members/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevBetterWeb.Core;
+using DevBetterWeb.Infrastructure.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBetterWeb.Web.Pages.Admin.Members
+{
+  [Authorize(Roles = AuthConstants.Roles.ADMINISTRATORS)]
+  public class IndexModel : PageModel
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public IndexModel(UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+      _userManager = userManager;
+      _roleManager = roleManager;
+    }
+
+    public List<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+    public List<UserWithRoles> UsersWithMemberRole { get; set; } = new List<UserWithRoles>();
+    public IdentityRole? Member { get; private set; } = new IdentityRole(AuthConstants.Roles.MEMBERS);
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+      Users = await _userManager.Users.ToListAsync();
+
+      foreach (var user in Users)
+      {
+        var roles = await _userManager.GetRolesAsync(user);
+
+        if (Member != null && roles.Contains(Member.Name))
+        {
+          var userWithRoles = new UserWithRoles(user, roles.ToList());
+          UsersWithMemberRole.Add(userWithRoles);
+        }
+      }
+
+
+      return Page();
+    }
+
+    public class UserWithRoles
+    {
+      public ApplicationUser User { get; set; }
+      public List<string> Roles { get; set; }
+
+      public UserWithRoles(ApplicationUser user, List<string> roles)
+      {
+        User = user;
+        Roles = roles;
+      }
+    }
+
+  }
+}
