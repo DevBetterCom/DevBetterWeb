d506ebf74152c2e3d06239b5d3f9255c2ff95c59
Rick Hodder <rickhodder@msn.com>
2021-06-09 04:49:47 -0400

    Fix Rank Calculations #313 (#316)
    * Fix Rank Calculations
    
    * Moved ranking to service in Core + tests
    
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

---
+++ src/DevBetterWeb.Core/Services/RankingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevBetterWeb.Core.Services
+{
+  public class RankingService<TKey> where TKey: notnull
+  {
+    public Dictionary<TKey, int> Rank(IEnumerable<TKey> items)
+    {
+      var result = new Dictionary<TKey, int>();
+
+      var counts = items
+        .Distinct()
+        .OrderByDescending(i=>i);
+
+      var rank = 1;
+      foreach (var count in counts)
+      {
+        result[count] = rank;
+        rank += 1;
+      }
+
+      return result;
+    }
+  }
+}
