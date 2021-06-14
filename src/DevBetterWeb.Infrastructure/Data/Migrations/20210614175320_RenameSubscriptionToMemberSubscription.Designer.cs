d3a29a37017ac38ae988be484ff7b97316f93d2c
Ilyana Smith <ilyana@kentsmiths.com>
2021-06-14 13:57:24 -0400

    subscription renamed to memberSubscription

--- src/DevBetterWeb.Infrastructure/Data/Migrations/20210614175320_RenameSubscriptionToMemberSubscription.Designer.cs
+++ src/DevBetterWeb.Infrastructure/Data/Migrations/20210614175320_RenameSubscriptionToMemberSubscription.Designer.cs
@@ -4,14 +4,16 @@ using DevBetterWeb.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevBetterWeb.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210614175320_RenameSubscriptionToMemberSubscription")]
+    partial class RenameSubscriptionToMemberSubscription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
