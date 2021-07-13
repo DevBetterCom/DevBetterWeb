9b989b80254c8a53f7be3e9cdf758bd41c10fd3f
Steve Smith <steve@kentsmiths.com>
2021-07-13 10:55:28 -0400

    Fixing sorting of progress circles Fixing errors locally with webhooks and invalid URLs

--- src/DevBetterWeb.Infrastructure/DiscordWebhooks/BookDiscussionWebhook.cs
+++ src/DevBetterWeb.Infrastructure/DiscordWebhooks/BookDiscussionWebhook.cs
@@ -1,17 +1,20 @@
 ﻿using Ardalis.GuardClauses;
+using DevBetterWeb.Core.Interfaces;
 using Microsoft.Extensions.Options;
 
 namespace DevBetterWeb.Infrastructure.DiscordWebooks
 {
   public class BookDiscussionWebhook : BaseWebhook
   {
-    public BookDiscussionWebhook(IOptions<DiscordWebhookUrls> optionsAccessor) : base(optionsAccessor.Value.BookDiscussion!)
+    public BookDiscussionWebhook(IOptions<DiscordWebhookUrls> optionsAccessor,
+      IAppLogger<BaseWebhook> logger) : base(optionsAccessor.Value.BookDiscussion!, logger)
     {
       Guard.Against.Null(optionsAccessor, nameof(optionsAccessor));
       Guard.Against.NullOrEmpty(optionsAccessor.Value.BookDiscussion, nameof(optionsAccessor.Value.BookDiscussion));
     }
 
-    public BookDiscussionWebhook(string webhookUrl) : base(webhookUrl)
+    public BookDiscussionWebhook(string webhookUrl,
+      IAppLogger<BaseWebhook> logger) : base(webhookUrl, logger)
     {
     }
   }
