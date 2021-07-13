45b0e4d08531242afbfd331606dd67f4d52a8163
Steve Smith <steve@kentsmiths.com>
2021-07-13 10:56:16 -0400

    Fixing sorting of progress circles (#357)
    Fixing errors locally with webhooks and invalid URLs

--- src/DevBetterWeb.Core/Interfaces/IAppLogger.cs
+++ src/DevBetterWeb.Core/Interfaces/IAppLogger.cs
@@ -1,12 +1,15 @@
+﻿using System;
+
 namespace DevBetterWeb.Core.Interfaces
 {
-    /// <summary>
-    /// This type eliminates the need to depend directly on the ASP.NET Core logging types.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public interface IAppLogger<T>
-    {
-        void LogInformation(string message, params object[] args);
-        void LogWarning(string message, params object[] args);
-    }
+  /// <summary>
+  /// This type eliminates the need to depend directly on the ASP.NET Core logging types.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public interface IAppLogger<T>
+  {
+    void LogInformation(string message, params object[] args);
+    void LogWarning(string message, params object[] args);
+    void LogError(Exception ex, string message, params object[] args);
+  }
 }
