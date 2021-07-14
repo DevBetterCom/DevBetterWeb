f0eb7bd9e70330710db56d85125593d1cb872d18
Steve Smith <steve@kentsmiths.com>
2021-07-14 16:00:26 -0400

    Initial resources page (#372)

---
+++ src/DevBetterWeb.Web/Pages/Resources/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using DevBetterWeb.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevBetterWeb.Web.Pages.Resources
+{
+  [Authorize(Roles = AuthConstants.Roles.ADMINISTRATORS_MEMBERS)]
+  public class IndexModel : PageModel
+  {
+    public IndexModel()
+    {
+    }
+
+    public async Task OnGet()
+    {
+    }
+  }
+}
