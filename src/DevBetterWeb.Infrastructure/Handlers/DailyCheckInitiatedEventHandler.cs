9a6d520ec0517fed02b8a571b0807db0e96f46c7
ilyanaDev <42644084+ilyanaDev@users.noreply.github.com>
2021-07-14 14:43:12 -0400

    Reminder emails for new signups (#371)
    * dates properties added to invitation entity
    
    * tests pass so far
    
    * tests pass
    
    * messages will now be added to the daily check in db
    
    * application user in interfaces deleted
    
    * CheckIfAnyActiveInvitationsRequireUserPing and CheckIfAnyActiveInvitationsRequireAdminPing removed from IDailyCheckPingService interface
    
    * inactiveInvitationByEmailSpec no longer partial
    
    * DailyCheckInitiatedEventHandler logic simplified
    
    * DailyCheckPingService InviteRequiresAdminPing updated
    
    * quick fixes
    
    * magic timespans removed
    
    * emailList and listOfEmails renamed
    
    * minor test refactoring

--- src/DevBetterWeb.Infrastructure/Handlers/DailyCheckInitiatedEventHandler.cs
+++ src/DevBetterWeb.Infrastructure/Handlers/DailyCheckInitiatedEventHandler.cs
@@ -1,20 +1,31 @@
-﻿using DevBetterWeb.Core.Events;
+﻿using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.Events;
 using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Core.Specs;
 using DevBetterWeb.Infrastructure.DiscordWebooks;
+using System;
 using System.Threading.Tasks;
 
 namespace DevBetterWeb.Core.Handlers
 {
   public class DailyCheckInitiatedEventHandler : IHandle<DailyCheckInitiatedEvent>
   {
+    private const string DAILY_CHECK_COMPLETED_MESSAGE = "Daily Check Event Completed";
+
     private readonly AdminUpdatesWebhook _webhook;
     private readonly IAlumniGraduationService _alumniGraduationService;
+    private readonly IDailyCheckPingService _dailyCheckPingService;
+    private readonly IRepository<DailyCheck> _repository;
 
     public DailyCheckInitiatedEventHandler(AdminUpdatesWebhook webhook,
-      IAlumniGraduationService alumniGraduationService)
+      IAlumniGraduationService alumniGraduationService,
+      IDailyCheckPingService dailyCheckPingService,
+      IRepository<DailyCheck> repository)
     {
       _webhook = webhook;
       _alumniGraduationService = alumniGraduationService;
+      _dailyCheckPingService = dailyCheckPingService;
+      _repository = repository;
     }
 
     public async Task Handle(DailyCheckInitiatedEvent domainEvent)
@@ -29,13 +40,39 @@ namespace DevBetterWeb.Core.Handlers
       await _alumniGraduationService.GraduateMembersIfNeeded(messages);
 
       // check if people need to be pinged about new member link
+      await _dailyCheckPingService.SendPingIfNeeded(messages);
 
-      messages.Append("Daily Check Event Completed");
+      messages.Append(DAILY_CHECK_COMPLETED_MESSAGE);
 
+      await SendMessagesToDiscord(messages);
+      await StoreMessagesInTasksCompleted(messages);
+    }
+
+    private async Task SendMessagesToDiscord(AppendOnlyStringList messages)
+    {
       foreach (var message in messages)
       {
         _webhook.Content = message;
         await _webhook.Send();
+      }
+    }
+
+    private async Task StoreMessagesInTasksCompleted(AppendOnlyStringList messages)
+    {
+      var spec = new DailyCheckByDateSpec(DateTime.Today);
+      var todaysDailyCheck = await _repository.GetBySpecAsync(spec);
+
+      if (todaysDailyCheck != null)
+      {
+        foreach (var message in messages)
+        {
+          if (!message.Equals(DAILY_CHECK_COMPLETED_MESSAGE))
+          {
+            if (todaysDailyCheck.TasksCompleted == null) todaysDailyCheck.TasksCompleted = "";
+            todaysDailyCheck.TasksCompleted += message;
+          }
+        }
+        await _repository.UpdateAsync(todaysDailyCheck);
       }
     }
   }
