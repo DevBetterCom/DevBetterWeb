ad7c07b19124bc1f246e181512d575c7fe5dc5de
Ilyana Smith <ilyana@kentsmiths.com>
2021-07-21 14:33:28 -0400

    number of subscription plans check Do not merge until stripe is live. Not a big deal as doing so shouldn't break anything, but daily check will send a lot of warnings

---
+++ src/DevBetterWeb.Infrastructure/Services/DailyCheckSubscriptionPlanCountService.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DevBetterWeb.Core;
+using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.Interfaces;
+
+namespace DevBetterWeb.Infrastructure.Services
+{
+  public class DailyCheckSubscriptionPlanCountService : IDailyCheckSubscriptionPlanCountService
+  {
+    private readonly IRepository<MemberSubscriptionPlan> _repository;
+    private readonly IPaymentHandlerPrice _paymentHandlerPrice;
+
+    private const int EXPECTED_NUMBER_OF_SUBSCRIPTION_PLANS_NOT_IN_PAYMENT_PROVIDER = 2;
+
+    public DailyCheckSubscriptionPlanCountService(IRepository<MemberSubscriptionPlan> repository,
+      IPaymentHandlerPrice paymentHandlerPrice)
+    {
+      _repository = repository;
+      _paymentHandlerPrice = paymentHandlerPrice;
+    }
+
+    public async Task WarnIfNumberOfMemberSubscriptionPlansDifferentThanExpected(AppendOnlyStringList messages)
+    {
+      var subscriptionPlans = await _repository.ListAsync();
+      int numberOfSubscriptionPlans = subscriptionPlans.Count();
+
+      var numberOfPaymentProviderSubscriptionPlans = await _paymentHandlerPrice.GetPriceCount();
+
+      if(numberOfSubscriptionPlans != numberOfPaymentProviderSubscriptionPlans + EXPECTED_NUMBER_OF_SUBSCRIPTION_PLANS_NOT_IN_PAYMENT_PROVIDER)
+      {
+        messages.Append("THE NUMBER OF MEMBERSUBSCRIPTIONPLANS DOES NOT MATCH THE EXPECTED VALUE");
+      }
+    }
+  }
+}
