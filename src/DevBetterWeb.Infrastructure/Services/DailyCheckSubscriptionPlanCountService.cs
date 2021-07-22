a42c208264dfdebdb2d1a762161aa77a98600e7a
Ilyana Smith <ilyana@kentsmiths.com>
2021-07-22 10:31:17 -0400

    fixed to be more flexible and use options

--- src/DevBetterWeb.Infrastructure/Services/DailyCheckSubscriptionPlanCountService.cs
+++ src/DevBetterWeb.Infrastructure/Services/DailyCheckSubscriptionPlanCountService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using DevBetterWeb.Core;
 using DevBetterWeb.Core.Entities;
 using DevBetterWeb.Core.Interfaces;
+using Microsoft.Extensions.Options;
 
 namespace DevBetterWeb.Infrastructure.Services
 {
@@ -10,18 +11,28 @@ namespace DevBetterWeb.Infrastructure.Services
   {
     private readonly IRepository<MemberSubscriptionPlan> _repository;
     private readonly IPaymentHandlerPrice _paymentHandlerPrice;
+    private readonly IOptions<SubscriptionPlanOptions> _optionsAccessor;
 
-    private const int EXPECTED_NUMBER_OF_SUBSCRIPTION_PLANS_NOT_IN_PAYMENT_PROVIDER = 2;
+    private int? EXPECTED_NUMBER_OF_SUBSCRIPTION_PLANS_NOT_IN_PAYMENT_PROVIDER;
 
     public DailyCheckSubscriptionPlanCountService(IRepository<MemberSubscriptionPlan> repository,
-      IPaymentHandlerPrice paymentHandlerPrice)
+      IPaymentHandlerPrice paymentHandlerPrice,
+      IOptions<SubscriptionPlanOptions> options)
     {
       _repository = repository;
       _paymentHandlerPrice = paymentHandlerPrice;
+      _optionsAccessor = options;
+      EXPECTED_NUMBER_OF_SUBSCRIPTION_PLANS_NOT_IN_PAYMENT_PROVIDER = _optionsAccessor.Value.expectedNumberOfSubscriptionPlansNotInPaymentProvider;
     }
 
     public async Task WarnIfNumberOfMemberSubscriptionPlansDifferentThanExpected(AppendOnlyStringList messages)
     {
+      // TODO this can be removed once stripe is live
+      if(!EXPECTED_NUMBER_OF_SUBSCRIPTION_PLANS_NOT_IN_PAYMENT_PROVIDER.HasValue || EXPECTED_NUMBER_OF_SUBSCRIPTION_PLANS_NOT_IN_PAYMENT_PROVIDER <= 0)
+      {
+        return;
+      }
+
       var subscriptionPlans = await _repository.ListAsync();
       int numberOfSubscriptionPlans = subscriptionPlans.Count();
 
