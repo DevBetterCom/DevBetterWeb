e64867a60327ef6e9f660d200bf053b8a22b0e08
Ilyana Smith <ilyana@kentsmiths.com>
2021-08-09 12:40:09 -0400

    add ef constructor for member subscription

--- src/DevBetterWeb.Core/Entities/MemberSubscription.cs
+++ src/DevBetterWeb.Core/Entities/MemberSubscription.cs
@@ -16,5 +16,12 @@ namespace DevBetterWeb.Core.Entities
       MemberSubscriptionPlanId = memberSubscriptionPlanId;
       Dates = dates;
     }
+
+    //for ef
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private MemberSubscription()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+    }
   }
 }
