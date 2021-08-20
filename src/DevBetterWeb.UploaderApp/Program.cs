cf36e52c2bd65ea974802938bc166cd59808c1ea
ShadyNagy <info@shadynagy.com>
2021-08-20 23:51:26 +0200

    DevBetterWeb.UploaderApp added.

---
+++ src/DevBetterWeb.UploaderApp/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DevBetterWeb.Vimeo.Models;
+using DevBetterWeb.Vimeo.Services.VideoServices;
+
+namespace DevBetterWeb.UploaderApp
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var argsList = args.ToList();
+      if (argsList.Count == 0 || argsList.All( x => x.ToLower() != "-d") || argsList.All(x => x.ToLower() != "-t"))
+      {
+        Console.WriteLine("PLease use -d [destination folder] -t [Vimeo token]");
+        return;
+      }
+
+      var folderToUploadIndex = argsList.FindIndex(x => x.ToLower() == "-d") + 1;
+      if(folderToUploadIndex <= 0)
+      {
+        Console.WriteLine("PLease use -d [destination folder]");
+        return;
+      }
+
+      var tokenIndex = argsList.FindIndex(x => x.ToLower() == "-t") + 1;
+      if (tokenIndex <= 0)
+      {
+        Console.WriteLine("PLease use -t [Vimeo token]");
+        return;
+      }
+
+      var folderToUpload = argsList[folderToUploadIndex];
+      var videos = GetVideos(folderToUpload);
+
+      var token = argsList[tokenIndex];
+      var httpService = Builders.BuildHttpService(token);
+      var uploader = Builders.BuildUploadVideoService(httpService);
+      foreach (var video in videos)
+      {
+        Console.WriteLine($"Starting Uploading {video.Name}");
+        var request = new UploadVideoRequest("me", video.Data, video);
+        request.FileData = video.Data;
+
+        var response = await uploader.ExecuteAsync(request);
+        if (response.Data > 0)
+        {
+          Console.WriteLine($"{video.Name} Uploaded");
+        }else
+        {
+          Console.WriteLine($"{video.Name} Upload Error");
+        }        
+      }
+
+      Console.WriteLine("Done, press any key to close");
+      Console.ReadKey();
+    }    
+
+    private static List<Video> GetVideos(string folderPath)
+    {
+      var result = new List<Video>();
+      if (!Directory.Exists(folderPath))
+      {
+        return result;
+      }
+
+      string[] videosPaths = Directory.GetFiles(folderPath, "*.mp4", SearchOption.AllDirectories);
+      if (videosPaths == null)
+      {
+        return result;
+      }
+
+      string[] mdsPaths = Directory.GetFiles(folderPath, "*.md", SearchOption.AllDirectories);
+      if (mdsPaths == null)
+      {
+        return result;
+      }
+
+      foreach (var videoPath in videosPaths)
+      {
+        var video = new Video();
+        video.SetName(Path.GetFileNameWithoutExtension(videoPath));
+
+        var mdFilePath = mdsPaths.FirstOrDefault(x => Path.GetFileNameWithoutExtension(x.ToLower().Trim()) == Path.GetFileNameWithoutExtension(videoPath.ToLower().Trim()));
+        video.SetDescription(File.ReadAllText(mdFilePath));
+
+        video.Data = File.ReadAllBytes(videoPath);
+        if (video.Data == null || video.Data.Length <= 0)
+        {
+          continue;
+        }
+        video.SetEmbedProtecedPrivacy();
+        result.Add(video);
+      }
+
+      return result;
+    }
+  }
+}
