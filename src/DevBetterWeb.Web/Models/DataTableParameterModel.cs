1397404ae9b42ec6644ecf4fe7e07e562e774961
Shady Nagy <shadynagi@gmail.com>
2021-09-08 22:26:38 +0200

    Videos pagination (#435)
    * DevBetterWeb.UploaderApp added.
    
    * fix sln
    
    * localhost removed from add domain.
    
    * check if video exist or not before upload added.
    
    * If no description add empty string.
    
    * Markdown added.
    
    * Markdown for Vimeo description.
    
    * Markdown add on details.
    
    * mdContent fixed on interface.
    
    * Show without MD file.
    
    * 2 hours timeout for Vimeo uploader app.
    
    * IMarkdown changed to IMarkdownService.
    
    * UploaderManager for console Vimeo uploader.
    
    * embedding atul_gawande_want_to_get_great_at_something_get_a_coach pn home
    
    * style videos list.
    
    * Fix hanging on stripe
    
    * fixed error message on stripe.
    
    * Clean fix for strpe hange
    
    * Button style fixed for submit.
    
    * button:hover added for submit.
    
    * Prevent duplicate subscriptions and charges via Stripe.
    
    * case-sensitive filter to get customer by email.
    
    * ApiCaller SetTimeout units renamed.
    
    * removed QueryBuilder.
    
    * DictionaryExtensions added.
    
    * SetHeaders added.
    
    * datetime format fixed.
    
    * refactor HttpService.
    
    * pagination added on videos.
    
    * Videos by DataTable.
    
    * fix table style.
    
    * Change function name to list on videos.
    
    * DataTableParameterModel added.
    
    * protect VideosController.
    
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

---
+++ src/DevBetterWeb.Web/Models/DataTableParameterModel.cs
@@ -0,0 +1,27 @@
+﻿namespace DevBetterWeb.Web.Models
+{
+  public class DataTableParameterModel
+  {
+    /// <summary>
+    /// Draw counter. This is used by DataTables to ensure that the Ajax returns from 
+    /// server-side processing requests are drawn in sequence by DataTables 
+    /// </summary>
+    public string Draw { get; set; }
+
+    /// <summary>
+    /// Paging first record indicator. This is the start point in the current data set 
+    /// (0 index based - i.e. 0 is the first record)
+    /// </summary>
+    public string Start { get; set; }
+
+    /// <summary>
+    /// Number of records that the table can display in the current draw. It is expected
+    /// that the number of records returned will be equal to this number, unless the 
+    /// server has fewer records to return. Note that this can be -1 to indicate that 
+    /// all records should be returned (although that negates any benefits of 
+    /// server-side processing!)
+    /// </summary>
+    public string Length { get; set; }
+
+  }
+}
--- src/DevBetterWeb.Web/wwwroot/js/videos.js
+++ src/DevBetterWeb.Web/wwwroot/js/videos.js
@@ -1,5 +1,48 @@
 ﻿$(document).ready(function () {
-
-   
-
+    var userRole = '@(User.IsInRole("Administrators") ? "true" : "false")';
+    var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    $("#videosDatatable").DataTable({
+        "processing": true,
+        "serverSide": true,
+        "filter": false,
+        "sort": false,
+        "ajax": {
+            "url": "/videos/list",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "columnDefs": [{
+            "targets": [0],
+            "visible": false,
+            "searchable": false
+        }],
+        "columns": [
+            { "data": "id", "name": "Id", "autoWidth": true },
+            { "data": "name", "name": "Name", "autoWidth": true },
+            {
+                "data": "releaseTime", "name": "Release Time", "autoWidth": true,
+                "render": function (data, type, full, meta) {
+                    return new Date(full.releaseTime).toLocaleDateString("en-US", dateOptions);
+                }
+            },
+            {
+                "data": "createdTime", "name": "Created Time", "autoWidth": true,
+                "render": function (data, type, full, meta) {
+                    return new Date(full.createdTime).toLocaleDateString("en-US", dateOptions);
+                }
+            },
+            { "data": "status", "name": "Status", "autoWidth": true },
+            {
+                "render": function (data, type, full, meta) {                    
+                    if (userRole) {
+                        return "<a href='Videos/Details/" + full.id + "'><i class='fas fa-info-circle'></i></a> |  <a href='Videos/Edit/" + full.id + "'><i class='fas fa-edit'></i></a> |<a href='Videos/Delete/" + full.id + "'><i class='far fa-trash-alt'></i></a>";
+                    } else {
+                        return "";
+                    }
+                        
+                   
+                }
+            },
+        ]
+    });
 });
