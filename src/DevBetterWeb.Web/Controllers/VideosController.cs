faa255607cfc81f12fd674fd1a811369d3ebc992
ShadyNagy <info@shadynagy.com>
2021-09-08 02:15:32 +0200

    Videos by DataTable.

---
+++ src/DevBetterWeb.Web/Controllers/VideosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DevBetterWeb.Vimeo.Services.VideoServices;
+using DevBetterWeb.Web.Models.Vimeo;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DevBetterWeb.Web.Controllers
+{
+  [Route("videos")]
+  [ApiController]
+  public class VideosController : Controller
+  {
+    private readonly IMapper _mapper;
+    private readonly GetAllVideosService _getAllVideosService;
+
+    public VideosController(IMapper mapper, GetAllVideosService getAllVideosService)
+    {
+      _mapper = mapper;
+      _getAllVideosService = getAllVideosService;
+    }
+
+    [HttpPost("list")]
+    public async Task<IActionResult> GetVideosAsync()
+    {
+      var draw = Request.Form["draw"].FirstOrDefault();      
+      var length = Request.Form["length"].FirstOrDefault();
+      int pageSize = length != null ? Convert.ToInt32(length) : 0;
+      var startIndex = Convert.ToInt32(Request.Form["start"].FirstOrDefault());
+      var start = startIndex == 0 ? 1 : (startIndex / pageSize)+1;
+
+      var request = new GetAllVideosRequest("me", start, pageSize);
+      var response = await _getAllVideosService
+        .ExecuteAsync(request);
+
+      var videoList = new List<VideoModel>();
+      if (response.Data?.Data != null)
+      {
+        videoList = _mapper.Map<List<VideoModel>>(response.Data.Data);
+      }
+
+      var jsonData = new { draw = draw, recordsFiltered = response.Data == null ? 0 : response.Data.Total, recordsTotal = response.Data == null ? 0 : response.Data.Total, data = videoList };
+
+      return Ok(jsonData);
+    }
+  }
+
+}
--- src/DevBetterWeb.Web/Pages/Videos/Index.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/Videos/Index.cshtml.cs
@@ -13,36 +13,5 @@ namespace DevBetterWeb.Web.Pages.Videos
   [Authorize(Roles = AuthConstants.Roles.ADMINISTRATORS_MEMBERS_ALUMNI)]
   public class IndexModel : PageModel
   {
-    private readonly IMapper _mapper;
-    private readonly GetAllVideosService _getAllVideosService;
-
-    [BindProperty]
-    public IList<VideoModel> VideoList { get; set; } = new List<VideoModel>();
-    [BindProperty]
-    public int PagesCount { get; set; } = 0;
-    [BindProperty]
-    public int SelectedPage { get; set; } = 1;
-
-
-    public IndexModel(IMapper mapper, GetAllVideosService getAllVideosService)
-    {
-      _mapper = mapper;
-      _getAllVideosService = getAllVideosService;
-    }
-
-    public async Task<IActionResult> OnGetAsync(int? pageIndex = 1, int? pageSize=10)
-    {
-      SelectedPage = pageIndex.Value;
-      var request = new GetAllVideosRequest("me", pageIndex, pageSize);
-      var response = await _getAllVideosService
-        .ExecuteAsync(request);
-
-      if (response.Data?.Data != null)
-      {
-        VideoList = _mapper.Map<List<VideoModel>>(response.Data.Data);
-        PagesCount = response.Data.Total/pageSize.Value;
-      }
-      return Page();
-    }
   }
 }
