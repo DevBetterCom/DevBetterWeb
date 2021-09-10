71e613270bf82032cf79d3279e639d12fc4994c4
Steve Smith <steve@kentsmiths.com>
2021-09-10 07:28:35 -0400

    add logging services

--- src/DevBetterWeb.Web/Startup.cs
+++ src/DevBetterWeb.Web/Startup.cs
@@ -61,6 +61,8 @@ namespace DevBetterWeb.Web
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
 
+      services.AddLogging();
+
       services.Configure<AuthMessageSenderOptions>(Configuration.GetSection("AuthMessageSenderOptions"));
       services.Configure<DiscordWebhookUrls>(Configuration.GetSection("DiscordWebhookUrls"));
       services.Configure<StripeOptions>(Configuration.GetSection("StripeOptions"));
@@ -117,7 +119,7 @@ namespace DevBetterWeb.Web
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
       });
 
-      //services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
+      services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
     }
 
     public void ConfigureContainer(ContainerBuilder builder)
