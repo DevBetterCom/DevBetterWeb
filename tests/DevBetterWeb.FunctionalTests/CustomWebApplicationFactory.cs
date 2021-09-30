1b48ec6bfd922fb01be283992959e040e4ab3d39
Steve Smith <steve@kentsmiths.com>
2021-09-30 14:12:20 -0400

    Add functional tests Add test for home page Fixes #438

---
+++ tests/DevBetterWeb.FunctionalTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using DevBetterWeb.Infrastructure.Data;
+using DevBetterWeb.Infrastructure.Identity.Data;
+using DevBetterWeb.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevBetterWeb.FunctionalTests
+{
+  public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+  {
+    /// <summary>
+    /// Overriding CreateHost to avoid creating a separate ServiceProvider per this thread:
+    /// https://github.com/dotnet-architecture/eShopOnWeb/issues/465
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+      var host = builder.Build();
+
+      // Get service provider.
+      var serviceProvider = host.Services;
+
+      // Create a scope to obtain a reference to the database
+      // context (AppDbContext).
+      using (var scope = serviceProvider.CreateScope())
+      {
+        var scopedServices = scope.ServiceProvider;
+        var db = scopedServices.GetRequiredService<AppDbContext>();
+        var userManager = scopedServices.GetRequiredService<UserManager<ApplicationUser>>();
+
+        var logger = scopedServices
+            .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+        // Ensure the database is created.
+        db.Database.EnsureCreated();
+
+        try
+        {
+          // Seed the database with test data.
+          SeedData.PopulateTestData(db, userManager);
+        }
+        catch (Exception ex)
+        {
+          logger.LogError(ex, "An error occurred seeding the " +
+                              $"database with test messages. Error: {ex.Message}");
+        }
+      }
+
+      host.Start();
+      return host;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+      builder
+          .UseSolutionRelativeContentRoot("src/DevBetterWeb.Web")
+          .ConfigureServices(services =>
+          {
+            // Remove the app's ApplicationDbContext registration.
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+            {
+              services.Remove(descriptor);
+            }
+
+            // This should be set for each individual test run
+            string inMemoryCollectionName = Guid.NewGuid().ToString();
+
+            // Add ApplicationDbContext using an in-memory database for testing.
+            services.AddDbContext<AppDbContext>(options =>
+            {
+              options.UseInMemoryDatabase(inMemoryCollectionName);
+            });
+
+            //services.AddScoped<IMediator, NoOpMediator>();
+          });
+    }
+  }
+}
---
+++ tests/DevBetterWeb.FunctionalTests/RazorPages/HomePageGet.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using DevBetterWeb.Web;
+using Xunit;
+
+namespace DevBetterWeb.FunctionalTests.RazorPages
+{
+  public class HomePageGet : IClassFixture<CustomWebApplicationFactory<Startup>>
+  {
+    private readonly HttpClient _client;
+
+    public HomePageGet(CustomWebApplicationFactory<Startup> factory)
+    {
+      _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task ReturnsViewWithCorrectMessage()
+    {
+      HttpResponseMessage response = await _client.GetAsync("/");
+      response.EnsureSuccessStatusCode();
+      string stringResponse = await response.Content.ReadAsStringAsync();
+
+      Assert.Contains("Steve has been a great coach and a source of precious practical advice and encouragement.", stringResponse);
+    }
+  }
+}
