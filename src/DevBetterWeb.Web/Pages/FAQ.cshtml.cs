3ca79a21a0795f4be0351c0d17c0bd2700c638d0
Steve Smith <steve@kentsmiths.com>
2021-10-01 09:53:37 -0400

    reverting to C# 9 namespace syntax

--- src/DevBetterWeb.Web/Pages/FAQ.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/FAQ.cshtml.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DevBetterWeb.Web.Pages;
-
-public class FAQModel : PageModel
+namespace DevBetterWeb.Web.Pages
 {
+  public class FAQModel : PageModel
+  {
     public void OnGet()
     {
     }
+  }
 }
