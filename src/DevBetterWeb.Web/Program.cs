8359b9d71b310722d9d2965c4d347d815dd5f9a4
ShadyNagy <info@shadynagy.com>
2021-11-09 21:06:24 +0200

    dep fixed.

--- src/DevBetterWeb.Web/Program.cs
+++ src/DevBetterWeb.Web/Program.cs
@@ -8,6 +8,7 @@ using DevBetterWeb.Infrastructure.Identity.Data;
 using DevBetterWeb.Web.Areas.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
