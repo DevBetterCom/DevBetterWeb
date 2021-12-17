a7ef26d3d551013e2997776e311155fb8b7f9684
Steve Smith <steve@kentsmiths.com>
2021-12-17 11:11:31 -0500

    Adding AppStartup event and webhook (#597)
    * Adding AppStartup event and webhook
    
    * remove _notificationSent
    
    * Refactor to its own file

---
+++ src/DevBetterWeb.Infrastructure/Handlers/AppStartedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.DiscordWebooks;
+
+namespace DevBetterWeb.Core.Handlers;
+
+public class AppStartedEventHandler : IHandle<AppStartedEvent>
+{
+  private readonly AdminUpdatesWebhook _webhook;
+
+  public AppStartedEventHandler(AdminUpdatesWebhook webhook)
+  {
+    _webhook = webhook;
+  }
+  
+  public async Task Handle(AppStartedEvent domainEvent)
+  {
+    var message = $"DevBetter.com web app started at {domainEvent.StartDateTime}.";
+    _webhook.Content = message;
+    await _webhook.Send();
+  }
+}
