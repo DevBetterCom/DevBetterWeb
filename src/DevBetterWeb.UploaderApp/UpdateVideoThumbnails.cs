2635d6882207d690b13643a830677d76dd7f4ad9
Shady Nagy <shadynagi@gmail.com>
2022-01-27 15:25:55 +0200

    Shady nagy/uploader issues (#632)
    * Endpoint UpdateVideoThumbnailsAsync added.
    
    * update animated thumbnails removed.
    
    * comman update-thumbnails added.
    
    * messages changed.
    
    * changes on controller
    
    * debug messages added.

---
+++ src/DevBetterWeb.UploaderApp/UpdateVideoThumbnails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiClient;
+
+namespace DevBetterWeb.UploaderApp;
+
+public class UpdateVideoThumbnails : BaseAsyncApiCaller
+  .WithRequest<ArchiveVideo>
+  .WithoutResponse
+{
+  private readonly HttpService _httpService;
+
+  public UpdateVideoThumbnails(HttpService httpService)
+  {
+    _httpService = httpService;
+  }
+
+  public override async Task<HttpResponse> ExecuteAsync(ArchiveVideo archiveVideo, CancellationToken cancellationToken = default)
+  {
+    var uri = $"videos/update-video-thumbnails";
+    try
+    {
+      var response = await _httpService.HttpPostAsync<ArchiveVideo>(uri, archiveVideo);
+
+      return HttpResponse.FromHttpResponseMessage(response.Code);
+    }
+    catch (Exception exception)
+    {
+      return HttpResponse.FromException(exception.Message);
+    }
+  }
+}
