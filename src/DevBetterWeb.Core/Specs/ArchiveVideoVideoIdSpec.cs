5929f5c8fce3616dc3fd1599db0bbbe7ccd2116d
ShadyNagy <info@shadynagy.com>
2022-02-12 21:14:01 +0200

    spec added for delete videos.

---
+++ src/DevBetterWeb.Core/Specs/ArchiveVideoVideoIdSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public class ArchiveVideoVideoIdSpec : Specification<ArchiveVideo>
+{
+  public ArchiveVideoVideoIdSpec()
+  {
+    Query
+      .Where(s => !string.IsNullOrEmpty(s.VideoId));
+  }
+}
