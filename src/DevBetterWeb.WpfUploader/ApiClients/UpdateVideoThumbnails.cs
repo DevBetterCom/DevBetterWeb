dbbae76a42b78bfb1558a2342b9df231741f8214
ShadyNagy <info@shadynagy.com>
2022-02-26 22:33:27 +0200

    WPF start.

---
+++ src/DevBetterWeb.WpfUploader/ApiClients/UpdateVideoThumbnails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiClient;
+using DevBetterWeb.WpfUploader.Models;
+
+namespace DevBetterWeb.WpfUploader.ApiClients;
+
+public class UpdateVideoThumbnails : BaseAsyncApiCaller
+  .WithRequest<ArchiveVideo>
+  .WithoutResponse
+{
+  private readonly HttpService _httpService;
+
+  public UpdateVideoThumbnails(HttpService httpService)
+  {
+    _httpService = httpService;
+  }
+
+  public override async Task<HttpResponse> ExecuteAsync(ArchiveVideo archiveVideo, CancellationToken cancellationToken = default)
+  {
+    var uri = $"videos/update-video-thumbnails";
+    try
+    {
+      var response = await _httpService.HttpPostAsync<ArchiveVideo>(uri, archiveVideo);
+
+      return HttpResponse.FromHttpResponseMessage(response.Code);
+    }
+    catch (Exception exception)
+    {
+      return HttpResponse.FromException(exception.Message);
+    }
+  }
+}
