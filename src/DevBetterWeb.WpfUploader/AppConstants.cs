c0d1d42db184020665d5a5671191e14bf098522b
ShadyNagy <info@shadynagy.com>
2022-06-13 00:28:35 +0200

    App Constants added.

---
+++ src/DevBetterWeb.WpfUploader/AppConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DevBetterWeb.WpfUploader;
+
+public static class AppConstants
+{
+  public static string TOKEN = "token";
+  public static string API_LINK = "apiLink";
+  public static string API_KEY = "apiKey";
+}
