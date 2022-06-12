856c9dad10016b525e6d33ed0f7a6862da40d887
ShadyNagy <info@shadynagy.com>
2022-06-13 00:28:01 +0200

    Update for Config Info added.

--- src/DevBetterWeb.WpfUploader/Models/ConfigInfo.cs
+++ src/DevBetterWeb.WpfUploader/Models/ConfigInfo.cs
@@ -2,11 +2,18 @@
 
 public class ConfigInfo
 {
-  public string Token { get; }
-  public string ApiLink { get; }
-  public string ApiKey { get; }
+  public string Token { get; private set; }
+  public string ApiLink { get; private set; }
+  public string ApiKey { get; private set; }
 
   public ConfigInfo(string token, string apiLink, string apiKey)
+  {
+    Token = token;
+    ApiLink = apiLink;
+    ApiKey = apiKey;
+  }
+
+  public void Update(string token, string apiLink, string apiKey)
   {
     Token = token;
     ApiLink = apiLink;
