7fbad5f79d3f5c20f8a6b83390353136f16b24dc
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2022-06-14 11:49:52 -0400

    Fix favorites count (#757)

---
+++ src/DevBetterWeb.Core/Specs/ArchiveVideoByVideoIdWithMemberFavoritesSpec.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public class ArchiveVideoByVideoIdWithMemberFavoritesSpec : Specification<ArchiveVideo>, ISingleResultSpecification
+{
+  public ArchiveVideoByVideoIdWithMemberFavoritesSpec(string videoId)
+  {
+    Query
+      .Where(x => x.VideoId == videoId)
+      .Include(X => X.MemberFavorites);
+  }
+}
