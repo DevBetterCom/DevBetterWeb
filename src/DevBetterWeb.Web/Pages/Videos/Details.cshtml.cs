356dd9080e62107ef60cf1ba57bdb2958e9aec1d
ShadyNagy <info@shadynagy.com>
2022-07-10 19:04:07 +0200

    Video details read the comments.

--- src/DevBetterWeb.Web/Pages/Videos/Details.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/Videos/Details.cshtml.cs
@@ -1,13 +1,17 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using DevBetterWeb.Core;
 using DevBetterWeb.Core.Entities;
 using DevBetterWeb.Core.Interfaces;
 using DevBetterWeb.Core.Specs;
 using DevBetterWeb.Infrastructure.Identity.Data;
 using DevBetterWeb.Vimeo.Services.VideoServices;
+using DevBetterWeb.Web.Models;
 using DevBetterWeb.Web.Pages.Admin.Videos;
+using Markdig.Renderers.Html;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -24,13 +28,15 @@ public class DetailsModel : PageModel
   private readonly GetOEmbedVideoService _getOEmbedVideoService;
   private readonly GetVideoService _getVideoService;
   private readonly IRepository<ArchiveVideo> _repository;
+  private readonly IMapper _mapper;
   private readonly IMarkdownService _markdownService;
   private readonly UserManager<ApplicationUser> _userManager;
   private readonly IRepository<Member> _memberRepository;
 
-  public DetailsModel(IMarkdownService markdownService, GetOEmbedVideoService getOEmbedVideoService, GetVideoService getVideoService, IRepository<ArchiveVideo> repository, UserManager<ApplicationUser> userManager, IRepository<Member> memberRepository)
+  public DetailsModel(IMapper mapper, IMarkdownService markdownService, GetOEmbedVideoService getOEmbedVideoService, GetVideoService getVideoService, IRepository<ArchiveVideo> repository, UserManager<ApplicationUser> userManager, IRepository<Member> memberRepository)
   {
-    _markdownService = markdownService;
+	  _mapper = mapper;
+	  _markdownService = markdownService;
     _getVideoService = getVideoService;
     _repository = repository;
     _getOEmbedVideoService = getOEmbedVideoService;
@@ -46,7 +52,7 @@ public class DetailsModel : PageModel
     var oEmbed = await _getOEmbedVideoService.ExecuteAsync(video.Data.Link);
     if (oEmbed?.Data == null) return NotFound($"Video Not Found {videoId}");
 
-    var videoSpec = new ArchiveVideoByVideoIdWithMemberFavoritesSpec(videoId);
+    var videoSpec = new ArchiveVideoByVideoIdWithMemberFavoritesAndCommentsSpec(videoId);
     var archiveVideo = await _repository.FirstOrDefaultAsync(videoSpec);
     if (archiveVideo == null) return NotFound($"Video Not Found {videoId}");
 
@@ -63,6 +69,7 @@ public class DetailsModel : PageModel
     OEmbedViewModel = new OEmbedViewModel(oEmbed.Data);
     OEmbedViewModel.VideoId = int.Parse(archiveVideo.VideoId!);
     OEmbedViewModel.Name = archiveVideo.Title;
+    OEmbedViewModel.Comments = _mapper.Map<List<VideoCommentDto>>(archiveVideo.Comments);
     OEmbedViewModel.Password = video.Data.Password;
     OEmbedViewModel.DescriptionMd = _markdownService.RenderHTMLFromMD(archiveVideo.Description);
     OEmbedViewModel.Description = archiveVideo.Description;
