4f48ad28010a4b7424a6c180299d67389f1ceb50
ShadyNagy <info@shadynagy.com>
2022-07-10 17:26:40 +0200

    changes on ArchiveVideo and Member for Video Comment.

--- src/DevBetterWeb.Core/Entities/VideoComment.cs
+++ src/DevBetterWeb.Core/Entities/VideoComment.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Ardalis.GuardClauses;
 using DevBetterWeb.Core.Interfaces;
 using DevBetterWeb.Core.SharedKernel;
 
@@ -7,12 +8,31 @@ namespace DevBetterWeb.Core.Entities;
 public class VideoComment : BaseEntity, IAggregateRoot
 {
   public string? VideoId { get; set; }
-  public string? ParentCommentId { get; set; }
+  public int? ParentCommentId { get; set; }
   public string? Body { get; set; }
   public int MemberId { get; set; }
-  public Member MemberWhoCreate { get; set; } = new Member();
-  public VideoComment ParentComment { get; set; } = new VideoComment();
+  public Member MemberWhoCreate { get; private set; } = new Member();
+  public VideoComment ParentComment { get; private set; } = new VideoComment();
+  public List<VideoComment> Replies { get; private set; } = new List<VideoComment>();
   public List<ArchiveVideo> Videos { get; set; } = new List<ArchiveVideo>();
+
+  public void AddReplay(VideoComment comment)
+  {
+	  Guard.Against.Null(comment, nameof(comment));
+	  Replies.Add(comment);
+  }
+
+  public void SetParentComment(VideoComment comment)
+  {
+	  Guard.Against.Null(comment, nameof(comment));
+	  ParentComment = comment;
+  }
+
+  public void SetCreator(Member member)
+  {
+	  Guard.Against.Null(member, nameof(member));
+	  MemberWhoCreate = member;
+  }
 
   public override string ToString()
   {
