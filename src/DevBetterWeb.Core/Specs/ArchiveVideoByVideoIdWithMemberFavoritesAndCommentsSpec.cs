dc597dae86f9c618c4e761ab87ef8466de20e2d4
ShadyNagy <info@shadynagy.com>
2022-07-10 19:03:02 +0200

    video comment specs added.

---
+++ src/DevBetterWeb.Core/Specs/ArchiveVideoByVideoIdWithMemberFavoritesAndCommentsSpec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public class ArchiveVideoByVideoIdWithMemberFavoritesAndCommentsSpec : Specification<ArchiveVideo>, ISingleResultSpecification
+{
+  public ArchiveVideoByVideoIdWithMemberFavoritesAndCommentsSpec(string videoId)
+  {
+    Query
+      .Where(x => x.VideoId == videoId)
+      .Include(X => X.MemberFavorites)
+      .Include(x => x.Comments);
+  }
+}
---
+++ src/DevBetterWeb.Core/Specs/VideoCommentByVideoIdSpec.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public class VideoCommentByVideoIdSpec : Specification<VideoComment>
+{
+  public VideoCommentByVideoIdSpec(string videoId)
+  {
+	  var videoIdToSearchBy = long.Parse(videoId);
+	  Query
+		  .Where(x => x.VideoId == videoIdToSearchBy)
+		  .Include(x => x.Replies)
+		  .OrderByDescending(x => x.CreatedAt);
+  }
+}
