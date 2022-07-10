50eb756d9b22f8da42e75e4e2f94bee805a87d2f
ShadyNagy <info@shadynagy.com>
2022-07-10 17:18:13 +0200

    VideoComment created.

---
+++ src/DevBetterWeb.Core/Entities/VideoComment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Core.SharedKernel;
+
+namespace DevBetterWeb.Core.Entities;
+
+public class VideoComment : BaseEntity, IAggregateRoot
+{
+  public string? VideoId { get; set; }
+  public string? ParentCommentId { get; set; }
+  public string? Body { get; set; }
+  public int MemberId { get; set; }
+  public Member MemberWhoCreate { get; set; } = new Member();
+  public VideoComment ParentComment { get; set; } = new VideoComment();
+  public List<ArchiveVideo> Videos { get; set; } = new List<ArchiveVideo>();
+
+  public override string ToString()
+  {
+    return Body!;
+  }
+}
