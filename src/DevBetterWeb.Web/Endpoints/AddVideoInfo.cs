3b6a3b52c1b53e040e936bfa09140db5e16ea37d
ShadyNagy <info@shadynagy.com>
2022-07-10 14:45:58 +0200

    uploader endpoints added.

---
+++ src/DevBetterWeb.Web/Endpoints/AddVideoInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using AutoMapper;
+using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Core.Specs;
+using DevBetterWeb.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using DevBetterWeb.Web.CustomAttributes;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+[IntegrationApiAuthorization]
+public class AddVideoInfo : EndpointBaseAsync
+	.WithRequest<ArchiveVideoDto>
+	.WithResult<ActionResult<ArchiveVideoDto>>
+{
+	private readonly IRepository<ArchiveVideo> _repository;
+	private readonly IMapper _mapper;
+
+	public AddVideoInfo(IRepository<ArchiveVideo> repository, IMapper mapper)
+	{
+		_repository = repository;
+		_mapper = mapper;
+	}
+
+	[HttpPost("videos/add-video-info")]
+	public override async Task<ActionResult<ArchiveVideoDto>> HandleAsync([FromBody] ArchiveVideoDto request, CancellationToken cancellationToken = default)
+	{
+		var archiveVideo = _mapper.Map<ArchiveVideo>(request);
+
+		var spec = new ArchiveVideoByVideoIdSpec(archiveVideo.VideoId!);
+		var existVideo = await _repository.FirstOrDefaultAsync(spec, cancellationToken);
+		if (existVideo == null)
+		{
+			archiveVideo = await _repository.AddAsync(archiveVideo, cancellationToken);
+			var videoAddedEvent = new VideoAddedEvent(archiveVideo);
+			archiveVideo.Events.Add(videoAddedEvent);
+		}
+		else
+		{
+			existVideo.Description = archiveVideo.Description;
+			existVideo.Title = archiveVideo.Title;
+			existVideo.Duration = archiveVideo.Duration;
+			if (!string.IsNullOrEmpty(archiveVideo.AnimatedThumbnailUri))
+			{
+				existVideo.AnimatedThumbnailUri = archiveVideo.AnimatedThumbnailUri;
+			}
+
+			await _repository.UpdateAsync(existVideo, cancellationToken);
+		}
+
+		return Ok(request);
+	}
+}
---
+++ src/DevBetterWeb.Web/Endpoints/DeleteAllVideosNoVimeo.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using DevBetterWeb.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using DevBetterWeb.Web.CustomAttributes;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+[IntegrationApiAuthorization]
+public class DeleteAllVideosNoVimeo : EndpointBaseAsync
+	.WithoutRequest
+	.WithoutResult
+{
+	private readonly IVideosService _videosService;
+
+	public DeleteAllVideosNoVimeo(IVideosService videosService)
+	{
+		_videosService = videosService;
+	}
+
+	[HttpGet("videos/delete-all-videos-no-vimeo")]
+	public override async Task<ActionResult> HandleAsync(CancellationToken cancellationToken = default)
+	{
+		await _videosService.DeleteVideosNotExistOnVimeoFromVimeo(null);
+		await _videosService.DeleteVideosNotExistOnVimeoFromDatabase(null);
+
+		return Ok();
+	}
+}
