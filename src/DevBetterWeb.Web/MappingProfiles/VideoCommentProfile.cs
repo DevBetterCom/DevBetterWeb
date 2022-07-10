b829403576556b3fe68e0a79341b217e6c13eac8
ShadyNagy <info@shadynagy.com>
2022-07-10 19:03:28 +0200

    mapper for video comment added.

---
+++ src/DevBetterWeb.Web/MappingProfiles/VideoCommentProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Web.Models;
+
+namespace DevBetterWeb.Web.MappingProfiles;
+
+public class VideoCommentProfile : Profile
+{
+  public VideoCommentProfile()
+  {
+	  CreateMap<VideoComment, VideoCommentDto>();
+		CreateMap<VideoCommentDto, VideoComment>();
+	}
+}
