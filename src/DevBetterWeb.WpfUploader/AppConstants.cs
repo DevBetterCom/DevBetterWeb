49874348e09320ab1c31832a410c90837c351889
ShadyNagy <info@shadynagy.com>
2022-07-11 21:16:07 +0200

    Uploaders removed.

--- src/DevBetterWeb.WpfUploader/AppConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevBetterWeb.WpfUploader;
-
-public static class AppConstants
-{
-  public static string TOKEN = "token";
-  public static string API_LINK = "apiLink";
-  public static string API_KEY = "apiKey";
-}
