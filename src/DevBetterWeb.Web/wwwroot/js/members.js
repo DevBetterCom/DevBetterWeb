64bd7b4f55e731f134176f8e25b44a4261d6b6bc
ShadyNagy <info@shadynagy.com>
2022-07-12 23:06:04 +0200

    All members checkbox.

--- src/DevBetterWeb.Web/wwwroot/js/members.js
+++ src/DevBetterWeb.Web/wwwroot/js/members.js
@@ -3,3 +3,17 @@
     $('#membersTable').DataTable({ "pageLength" : 25 });
 
 });
+
+$(document).ready(function() {
+	showAllMembersFunc();
+
+	$('#showAllMembers').change(function() {
+		showAllMembersFunc();
+	});
+});
+
+function showAllMembersFunc() {
+	var showAllMembers = document.getElementById("showAllMembers").checked;
+	var table = $('#membersTable').DataTable();
+	table.column(4).search( showAllMembers?"":"true" ).draw();
+}
