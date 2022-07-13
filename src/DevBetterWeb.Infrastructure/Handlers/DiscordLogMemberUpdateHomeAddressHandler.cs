0e2046255c5196c1679c62fbe3e938215cf94876
ShadyNagy <info@shadynagy.com>
2022-07-13 22:45:14 +0200

    Send discord message omn admin channel when address changed.

---
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogMemberUpdateHomeAddressHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.DiscordWebooks;
+
+namespace DevBetterWeb.Core.Handlers;
+
+public class DiscordLogMemberUpdateHomeAddressHandler : IHandle<MemberHomeAddressUpdatedEvent>
+{
+  private readonly AdminUpdatesWebhook _webhook;
+
+  public DiscordLogMemberUpdateHomeAddressHandler(AdminUpdatesWebhook webhook)
+  {
+    _webhook = webhook;
+  }
+
+  public Task Handle(MemberHomeAddressUpdatedEvent memberHomeAddressUpdatedEvent)
+  {
+    _webhook.Content = returnWebhookMessageString(memberHomeAddressUpdatedEvent);
+    return _webhook.Send();
+  }
+
+  public static string returnWebhookMessageString(MemberHomeAddressUpdatedEvent memberHomeAddressUpdatedEvent)
+  {
+    return $"User {memberHomeAddressUpdatedEvent.Member.FirstName} {memberHomeAddressUpdatedEvent.Member.LastName} just updated their profile. " +
+					 $"Home Address to {memberHomeAddressUpdatedEvent.UpdateDetails}. " +
+        $"Check it out here: https://devbetter.com/User/Details/{memberHomeAddressUpdatedEvent.Member.UserId} ";
+  }
+}
