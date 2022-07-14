566b9d2a7ea41f246db61c2d77a8af8a661f8ced
ShadyNagy <info@shadynagy.com>
2022-07-14 04:07:26 +0200

    mapper and DTO for book.

---
+++ src/DevBetterWeb.Web/Models/BookDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DevBetterWeb.Web.Models;
+
+public class BookDto
+{
+	public int? Id { get; set; }
+	public string? Title { get; set; }
+	public string? Author { get; set; }
+	public string? Details { get; set; }
+	public string? PurchaseUrl { get; set; }
+	public int BookCategoryId { get; set; }
+	public string? CategoryTitle { get; set; }
+	public int MembersWhoHaveReadCount { get; set; }
+}
