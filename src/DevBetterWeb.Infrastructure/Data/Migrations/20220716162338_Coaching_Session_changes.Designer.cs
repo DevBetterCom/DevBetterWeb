114a59afd6b08108cb3deb98d8192eee73180ff4
ShadyNagy <info@shadynagy.com>
2022-07-16 21:33:43 +0200

    Migrations.

--- src/DevBetterWeb.Infrastructure/Data/Migrations/20220716162338_Coaching_Session_changes.Designer.cs
+++ src/DevBetterWeb.Infrastructure/Data/Migrations/20220716162338_Coaching_Session_changes.Designer.cs
@@ -4,6 +4,7 @@ using DevBetterWeb.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevBetterWeb.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220716162338_Coaching_Session_changes")]
+    partial class Coaching_Session_changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
