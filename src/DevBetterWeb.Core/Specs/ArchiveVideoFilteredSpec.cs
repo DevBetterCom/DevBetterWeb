16fb23a717d88bffb63a87f183e608772ec8adfb
Shady Nagy <info@shadynagy.com>
2022-08-02 22:54:35 +0200

    Shady nagy/search paging (#827)
    * refactor.
    
    * Fixed.

--- src/DevBetterWeb.Core/Specs/ArchiveVideoFilteredSpec.cs
+++ src/DevBetterWeb.Core/Specs/ArchiveVideoFilteredSpec.cs
@@ -3,19 +3,21 @@ using DevBetterWeb.Core.Entities;
 
 namespace DevBetterWeb.Core.Specs;
 
-public class ArchiveVideoFilteredSpec : Specification<ArchiveVideo>
+public sealed class ArchiveVideoFilteredSpec : Specification<ArchiveVideo>
 {
   public ArchiveVideoFilteredSpec(string? search)
   {
     if (string.IsNullOrEmpty(search))
     {
-      Query.OrderByDescending(x => x.DateCreated);
+      Query
+	      .OrderByDescending(x => x.DateCreated);
     }
     else
     {
       Query
-        .Where(s => s.Description != null && !string.IsNullOrEmpty(search) && s.Description.Contains(search))
-          .OrderByDescending(x => x.DateCreated);
+        .Where(s => (s.Description != null && s.Description.Contains(search)) || 
+                    (s.Title != null && s.Title.Contains(search)))
+        .OrderByDescending(x => x.DateCreated);
     }
   }
 }
--- src/DevBetterWeb.Web/wwwroot/js/videos-members.js
+++ src/DevBetterWeb.Web/wwwroot/js/videos-members.js
@@ -2,6 +2,7 @@
 recordsTotal = 0;
 page = 1;
 recordsPage = 12;
+$pagination = $('#videoPagination');
 
 $(document).ready(function () {
     refreshMembersVideos(currentPage);
