6c3b3a5f24268132e54382e5ff832648b5cec3b8
ShadyNagy <info@shadynagy.com>
2022-08-06 16:38:15 +0200

    Migration added for the email.

--- src/DevBetterWeb.Infrastructure/Data/Migrations/20220806143642_email_in_member.Designer.cs
+++ src/DevBetterWeb.Infrastructure/Data/Migrations/20220806143642_email_in_member.Designer.cs
@@ -4,6 +4,7 @@ using DevBetterWeb.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevBetterWeb.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220806143642_email_in_member")]
+    partial class email_in_member
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
