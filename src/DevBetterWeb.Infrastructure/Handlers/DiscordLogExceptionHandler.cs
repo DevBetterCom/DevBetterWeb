99a219879f49d3473093fb99c2b77ff14e0b8a75
ShadyNagy <info@shadynagy.com>
2022-08-11 23:25:30 +0200

    loogger for regiser.

---
+++ src/DevBetterWeb.Infrastructure/Handlers/DiscordLogExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.DiscordWebooks;
+
+namespace DevBetterWeb.Core.Handlers;
+
+public class DiscordLogExceptionHandler : IHandle<ExceptionEvent>
+{
+  private readonly AdminUpdatesWebhook _webhook;
+
+  public DiscordLogExceptionHandler(AdminUpdatesWebhook webhook)
+  {
+    _webhook = webhook;
+  }
+
+  public Task Handle(ExceptionEvent domainEvent)
+  {
+    _webhook.Content = $"Exception {DateTime.UtcNow}: {domainEvent.Exception}";
+    return _webhook.Send();
+  }
+}
