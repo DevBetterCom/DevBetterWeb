ac7f690ca3b030f00d854009c3154f3968c48aa3
ShadyNagy <info@shadynagy.com>
2022-08-14 15:23:29 +0200

    TUS services created.

---
+++ src/DevBetterWeb.Vimeo/Services/VideoServices/UploadResumableVideoService.UploadVideoResumableRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace DevBetterWeb.Vimeo.Services.VideoServices;
+
+public class UploadVideoResumableInfo
+{
+	public byte[] FilePartData { get; set; }
+	public int FileFullSize { get; set; }
+	public string UploadUrl { get; set; }
+	public string FileName { get; set; }
+	public int UploadOffset { get; set; }
+}
