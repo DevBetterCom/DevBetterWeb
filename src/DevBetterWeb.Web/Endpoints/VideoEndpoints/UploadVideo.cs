36ffdcff22dfa8f76000b8652ece24bf63320ccf
ShadyNagy <info@shadynagy.com>
2022-08-14 15:25:19 +0200

    Edpoints to receive the TUS.

---
+++ src/DevBetterWeb.Web/Endpoints/VideoEndpoints/UploadVideo.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiClient;
+using Ardalis.ApiEndpoints;
+using AutoMapper;
+using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Core.Specs;
+using DevBetterWeb.Vimeo.Services.VideoServices;
+using DevBetterWeb.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using DevBetterWeb.Web.CustomAttributes;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+[UploaderApiAuthorization]
+public class UploadVideo : EndpointBaseAsync
+	.WithRequest<UploadVideoResumableInfo>
+	.WithResult<ActionResult<UploadVideoResumableInfo>>
+{
+	private readonly UploadResumableVideoService _uploadResumableVideoService;
+
+	public UploadVideo(UploadResumableVideoService uploadResumableVideoService)
+	{
+		_uploadResumableVideoService = uploadResumableVideoService;
+	}
+
+	[HttpPost("videos/upload-video")]
+	public override async Task<ActionResult<UploadVideoResumableInfo>> HandleAsync([FromBody] UploadVideoResumableInfo request, CancellationToken cancellationToken = default)
+	{
+		var result = await _uploadResumableVideoService.ExecuteAsync(request, cancellationToken);
+
+		return Ok(result?.Data);
+	}
+}
