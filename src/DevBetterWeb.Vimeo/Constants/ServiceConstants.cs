e97d7499760807bc623bb222c6b217ec172753ea
ShadyNagy <info@shadynagy.com>
2022-08-14 15:22:21 +0200

    Upgrade VIMEO_HTTP_ACCEPT to version 3.4.

--- src/DevBetterWeb.Vimeo/Constants/ServiceConstants.cs
+++ src/DevBetterWeb.Vimeo/Constants/ServiceConstants.cs
@@ -4,5 +4,5 @@ public static class ServiceConstants
 {
   public static string ME = "me";
   public static string VIMEO_URI = "https://api.vimeo.com";
-  public static string VIMEO_HTTP_ACCEPT = "application/vnd.vimeo.*+json; version=3.2, application/json";
+  public static string VIMEO_HTTP_ACCEPT = "application/vnd.vimeo.*+json; version=3.4, application/json";
 }
