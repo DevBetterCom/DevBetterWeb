4bb06b15195e6c84ac85413b27507d017d40c9e6
ShadyNagy <info@shadynagy.com>
2022-08-14 15:24:32 +0200

    TUS Model added.

---
+++ src/DevBetterWeb.Vimeo/Models/VideoForCreateResumableLink.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DevBetterWeb.Vimeo.Models;
+
+public class VideoForCreateResumableLink
+{
+
+	[JsonPropertyName("upload")]
+  public Upload Upload { get; set; }
+}
