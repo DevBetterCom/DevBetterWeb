90623466eee8d3cd2160db45ab6cb44ab46c2726
ShadyNagy <info@shadynagy.com>
2022-08-15 04:29:07 +0200

    Paged builder created for test.

---
+++ tests/DevBetterWeb.Vimeo.Tests/Builders/GetPagedVideosServiceBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.ApiClient;
+using DevBetterWeb.Vimeo.Services.VideoServices;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace DevBetterWeb.Vimeo.Tests.Builders;
+
+public class GetPagedVideosServiceBuilder
+{
+  public static GetPagedVideosService Build(HttpService httpService)
+  {
+    var logger = new Mock<ILogger<GetPagedVideosService>>().Object;
+
+    return new GetPagedVideosService(httpService, logger);
+  }
+}
