d386b39ffe600c67e7cc477cfb5fd68861bd6fec
ShadyNagy <ShadyNagi@gmail.com>
2022-08-16 15:30:44 +0200

    book category and uploaded by added.

--- src/DevBetterWeb.Web/Pages/Admin/Books/BookViewModel.cs
+++ src/DevBetterWeb.Web/Pages/Admin/Books/BookViewModel.cs
@@ -5,9 +5,22 @@ namespace DevBetterWeb.Web.Pages.Admin.Books;
 public class BookViewModel
 {
   public int Id { get; set; }
+
+	[Required]
   public string? Title { get; set; }
-  public string? Author { get; set; }
-  public string? Details { get; set; }
+
+	[Required]
+	public string? Author { get; set; }
+	[Display(Name = "Category")]
+	public string? CategoryTitle { get; set; }
+
+	[Required]
+	[Range(1, 100, ErrorMessage = "Book Category is required")]
+	[Display(Name = "Book Category")]
+	public int? BookCategoryId { get; set; }
+
+	public string? Details { get; set; }
+	public int? MemberWhoUploadId { get; set; }
 
 	[Display(Name = "Purchase Url")]
 	public string? PurchaseUrl { get; set; }
