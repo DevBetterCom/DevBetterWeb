3d64ec3c97d47453a79af69752b551367351c390
ShadyNagy <ShadyNagi@gmail.com>
2022-08-16 13:36:30 +0200

    Spec to get list of books for admin manage books.

---
+++ src/DevBetterWeb.Core/Specs/BooksWithMemberUploadedSpec.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public sealed class BooksWithMemberUploadedSpec : Specification<Book>
+{
+  public BooksWithMemberUploadedSpec()
+  {
+    Query
+      .OrderBy(book => book.Title ?? string.Empty)
+			.Include(book => book.MemberWhoUpload);		    
+	}
+}
