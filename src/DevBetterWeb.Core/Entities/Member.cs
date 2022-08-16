faecdd108df534b42cbe3fbdc474918d4aad209c
ShadyNagy <ShadyNagi@gmail.com>
2022-08-16 11:45:44 +0200

    Book uploader added.

--- src/DevBetterWeb.Core/Entities/Member.cs
+++ src/DevBetterWeb.Core/Entities/Member.cs
@@ -63,6 +63,7 @@ public class Member : BaseEntity, IAggregateRoot
   public int BooksRank { get; private set; }
 
   public List<Book> BooksRead { get; set; } = new List<Book>();
+  public List<Book> UploadedBooks { get; set; } = new List<Book>();
   public List<MemberVideoProgress> MemberVideosProgress { get; set; } = new List<MemberVideoProgress>();
   public List<VideoComment> VideosComments { get; set; } = new List<VideoComment>();
   public List<Question> Questions { get; set; } = new List<Question>();
