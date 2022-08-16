b9f603c74f1fa906e40063d7d7defdab6dd83216
ShadyNagy <ShadyNagi@gmail.com>
2022-08-16 15:33:06 +0200

    event and handler added to Discord when the book added by Alumni.

---
+++ src/DevBetterWeb.Core/Events/MemberAddedBookAddEvent.cs
@@ -0,0 +1,16 @@
+﻿using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.SharedKernel;
+
+namespace DevBetterWeb.Core.Events;
+
+public class MemberAddedBookAddEvent : BaseDomainEvent
+{
+  public MemberAddedBookAddEvent(Member member, Book book)
+  {
+    Member = member;
+    Book = book;
+  }
+
+  public Member Member { get; }
+  public Book Book { get; }
+}
