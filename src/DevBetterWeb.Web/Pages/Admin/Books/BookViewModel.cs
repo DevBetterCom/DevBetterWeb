3e404d937be8e86c44f2f282d356e16f4c17314d
ShadyNagy <ShadyNagi@gmail.com>
2022-08-16 13:35:44 +0200

    Upploaded by added on BookViewModel for admin books.

--- src/DevBetterWeb.Web/Pages/Admin/Books/BookViewModel.cs
+++ src/DevBetterWeb.Web/Pages/Admin/Books/BookViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace DevBetterWeb.Web.Pages.Admin.Books;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevBetterWeb.Web.Pages.Admin.Books;
 
 public class BookViewModel
 {
+  public int Id { get; set; }
   public string? Title { get; set; }
   public string? Author { get; set; }
   public string? Details { get; set; }
-  public string? PurchaseUrl { get; set; }
+
+	[Display(Name = "Purchase Url")]
+	public string? PurchaseUrl { get; set; }
+
+	[Display(Name = "Uploaded By")]
+  public string? MemberWhoUploaded { get; set; }
 }
