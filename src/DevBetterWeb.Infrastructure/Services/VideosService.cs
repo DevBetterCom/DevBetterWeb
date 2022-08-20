b7f85462389b04418b93d06fb6e3570d5ee8a825
ShadyNagy <info@shadynagy.com>
2022-08-20 04:07:42 +0200

    added AddArchiveVideoInfo and UPdateThumb to Video service.

--- src/DevBetterWeb.Infrastructure/Services/VideosService.cs
+++ src/DevBetterWeb.Infrastructure/Services/VideosService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using DevBetterWeb.Core;
 using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.Events;
 using DevBetterWeb.Core.Interfaces;
 using DevBetterWeb.Core.Specs;
 using DevBetterWeb.Vimeo.Services.VideoServices;
@@ -35,35 +37,17 @@ public class VideosService : IVideosService
 
   }
 
-  public async Task UpdateVideosThumbnail(AppendOnlyStringList? messages)
+  public async Task UpdateVideosThumbnail(AppendOnlyStringList? messages, CancellationToken cancellationToken = default)
   {
     var spec = new ArchiveVideoWithoutThumbnailSpec();
-    var videos = await _repositoryArchiveVideo.ListAsync(spec);
+    var videos = await _repositoryArchiveVideo.ListAsync(spec, cancellationToken);
     foreach (var video in videos)
     {
       try
       {
-        var response = await _getVideoService.ExecuteAsync(video.VideoId);
-        if (response?.Data == null)
-        {
-          continue;
-        }
-        var existThumbsResponse = await _getAllAnimatedThumbnailService.ExecuteAsync(new GetAnimatedThumbnailRequest(long.Parse(video.VideoId!), null));
-        if (existThumbsResponse.Data.Total <= 0)
-        {
-          var getAnimatedThumbnailResult = await _createAnimatedThumbnailsService.ExecuteAsync(long.Parse(video.VideoId!));
-          if (getAnimatedThumbnailResult == null || response?.Data.IsPlayable == false)
-          {
-            continue;
-          }
-          video.AnimatedThumbnailUri = getAnimatedThumbnailResult.AnimatedThumbnailUri;
-        }
-        else
-        {
-          video.AnimatedThumbnailUri = existThumbsResponse.Data.Data.FirstOrDefault()?.AnimatedThumbnailUri;
-        }
+	      _ = long.TryParse(video.VideoId, out var videoId);
+	      await UpdateVideoThumbnailsAsync(videoId, cancellationToken);
 
-        await _repositoryArchiveVideo.UpdateAsync(video);
         messages?.Append($"Video {video.VideoId} updated with Thumbnails.");
       }
       catch (Exception ex)
@@ -138,5 +122,72 @@ public class VideosService : IVideosService
         _logger.LogError(ex, $"Error on Delete Video from vimeo {video.Id}: {ex.Message}");
       }
     }
+  }
+
+  public async Task AddArchiveVideoInfo(ArchiveVideo archiveVideo, CancellationToken cancellationToken = default)
+  {
+	  var spec = new ArchiveVideoByVideoIdSpec(archiveVideo.VideoId!);
+	  var existVideo = await _repositoryArchiveVideo.FirstOrDefaultAsync(spec, cancellationToken);
+	  if (existVideo == null)
+	  {
+		  var videoAddedEvent = new VideoAddedEvent(archiveVideo);
+		  archiveVideo.Events.Add(videoAddedEvent);
+		  _ = await _repositoryArchiveVideo.AddAsync(archiveVideo, cancellationToken);
+	  }
+	  else
+	  {
+		  existVideo.Description = archiveVideo.Description;
+		  existVideo.Title = archiveVideo.Title;
+		  existVideo.Duration = archiveVideo.Duration;
+		  if (!string.IsNullOrEmpty(archiveVideo.AnimatedThumbnailUri))
+		  {
+			  existVideo.AnimatedThumbnailUri = archiveVideo.AnimatedThumbnailUri;
+		  }
+
+		  await _repositoryArchiveVideo.UpdateAsync(existVideo, cancellationToken);
+	  }
+	}
+
+  public async Task<ArchiveVideo?> UpdateVideoThumbnailsAsync(long videoId, CancellationToken cancellationToken = default)
+  {
+	  if (videoId <= 0)
+	  {
+		  return null;
+	  }
+
+	  var response = await _getVideoService.ExecuteAsync(videoId.ToString(), cancellationToken);
+	  if (response?.Data == null)
+	  {
+			return null;
+		}
+
+	  var existThumbsResponse = await _getAllAnimatedThumbnailService.ExecuteAsync(new GetAnimatedThumbnailRequest(videoId, null), cancellationToken);
+	  var animatedThumbnailUri = string.Empty;
+
+		if (existThumbsResponse.Data.Total <= 0)
+	  {
+		  var getAnimatedThumbnailResult = await _createAnimatedThumbnailsService.ExecuteAsync(videoId, cancellationToken);
+		  if (getAnimatedThumbnailResult == null)
+		  {
+				return null;
+			}
+		  animatedThumbnailUri = getAnimatedThumbnailResult.AnimatedThumbnailUri;
+	  }
+	  else
+	  {
+		  animatedThumbnailUri = existThumbsResponse.Data.Data.FirstOrDefault()?.AnimatedThumbnailUri;
+	  }
+		var spec = new ArchiveVideoByVideoIdSpec(videoId.ToString());
+	  var existVideo = await _repositoryArchiveVideo.FirstOrDefaultAsync(spec, cancellationToken);
+	  if (existVideo == null)
+	  {
+		  return null;
+	  }
+
+	  existVideo.AnimatedThumbnailUri = animatedThumbnailUri;
+  
+	  await _repositoryArchiveVideo.UpdateAsync(existVideo, cancellationToken);
+
+	  return existVideo;
   }
 }
