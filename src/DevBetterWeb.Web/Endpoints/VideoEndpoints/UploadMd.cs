a261db842f0cfbd47d20aaac08fe48c17082484d
ShadyNagy <info@shadynagy.com>
2022-08-20 19:03:18 +0200

    Md and subtitle endpoints added.

---
+++ src/DevBetterWeb.Web/Endpoints/VideoEndpoints/UploadMd.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiClient;
+using Ardalis.ApiEndpoints;
+using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Core.Specs;
+using Microsoft.AspNetCore.Mvc;
+using DevBetterWeb.Web.CustomAttributes;
+using DevBetterWeb.Web.Models;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+//TODO: need to add administrator here.
+[UploaderApiAuthorization]
+public class UploadMd : EndpointBaseAsync
+	.WithRequest<UploadMdRequest>
+	.WithResult<HttpResponse<bool>>
+{
+	private readonly IRepository<ArchiveVideo> _archiveVideoRepository;
+
+	public UploadMd(IRepository<ArchiveVideo> archiveVideoRepository)
+	{
+		_archiveVideoRepository = archiveVideoRepository;
+	}
+
+	[HttpPost("videos/upload-md")]
+	public override async Task<HttpResponse<bool>> HandleAsync([FromBody] UploadMdRequest request, CancellationToken cancellationToken = default)
+	{
+		var spec = new ArchiveVideoByVideoIdSpec(request.VideoId!);
+		var existVideo = await _archiveVideoRepository.FirstOrDefaultAsync(spec, cancellationToken);
+		if (existVideo == null)
+		{
+			return new HttpResponse<bool>(false, HttpStatusCode.NotFound);
+		}
+
+		existVideo.Description = request.Md;
+		await _archiveVideoRepository.UpdateAsync(existVideo, cancellationToken);
+
+		return new HttpResponse<bool>(true, HttpStatusCode.OK);
+	}
+}
--- src/DevBetterWeb.Web/Models/UploadSubtitleRequest.cs
+++ src/DevBetterWeb.Web/Models/UploadSubtitleRequest.cs
@@ -2,7 +2,7 @@
 
 public class UploadSubtitleRequest
 {
-  public string? VideoId { get; set; }
-  public string? Subtitle { get; set; }
+	public string VideoId { get; set; } = "0";
+	public string? Subtitle { get; set; }
   public string? Language { get; set; }
 }
