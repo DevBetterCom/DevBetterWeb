95ca1bc61ac6fba47da2b01809c6509097cae520
ShadyNagy <info@shadynagy.com>
2022-08-27 01:26:41 +0200

    remove all calc functions for rank from book category.

--- src/DevBetterWeb.Core/Entities/BookCategory.cs
+++ src/DevBetterWeb.Core/Entities/BookCategory.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using DevBetterWeb.Core.Interfaces;
-using DevBetterWeb.Core.Services;
 using DevBetterWeb.Core.SharedKernel;
 
 namespace DevBetterWeb.Core.Entities;
@@ -11,72 +10,7 @@ public class BookCategory : BaseEntity, IAggregateRoot
   public string? Title { get; set; }
   public List<Book> Books { get; private set; } = new List<Book>();
 
-  public void CalcAndSetBooksRank()
-  {
-		Book.CalcAndSetRank(Books);
-  }
-
-  public void CalcAndSetMembersRank()
-  {
-	  Member.CalcAndSetBooksRank(Books.SelectMany(x => x.MembersWhoHaveRead!).ToList());
-  }
-
-  public void ExcludeMembers(List<int> excludedMembersIds)
-  {
-	  foreach (var book in Books)
-	  {
-		  var newMembers = book.MembersWhoHaveRead!.Where(memberWhoHaveRead => !excludedMembersIds.Contains(memberWhoHaveRead.Id)).ToList();
-		  book.MembersWhoHaveRead = newMembers.OrderBy(x => x.BooksRank).ToList();
-		}
-  }
-	public void AddMembersRole(List<int> excludedMembersIds)
-	{
-		foreach (var book in Books.Where(book => book.MembersWhoHaveRead != null))
-		{
-			foreach(var memberWhoHaveRead in book.MembersWhoHaveRead!)
-			{
-				memberWhoHaveRead.SetRoleName(!excludedMembersIds.Contains(memberWhoHaveRead.Id)
-					? AuthConstants.Roles.MEMBERS
-					: AuthConstants.Roles.ALUMNI);
-			}
-		}
-	}
-	
-
-	public static void CalcAndSetCategoriesBooksRank(List<BookCategory> bookCategories)
-  {
-	  foreach (var bookCategory in bookCategories)
-	  {
-		  bookCategory.CalcAndSetBooksRank();
-	  }
-	}
-
-  public static void CalcAndSetMemberCategoriesMembersRank(List<BookCategory> bookCategories)
-  {
-	  foreach (var bookCategory in bookCategories)
-	  {
-		  bookCategory.CalcAndSetMembersRank();
-	  }
-  }
-
-  public static void ExcludeMembers(List<BookCategory> bookCategories, List<int> excludedMembersIds)
-  {
-	  foreach (var bookCategory in bookCategories)
-	  {
-		  bookCategory.ExcludeMembers(excludedMembersIds);
-	  }
-	}
-
-	public static void AddMembersRole(List<BookCategory> bookCategories, List<int> excludedMembersIds)
-	{
-		foreach (var bookCategory in bookCategories)
-		{
-			bookCategory.AddMembersRole(excludedMembersIds);
-		}
-	}
-
-
-	public override string ToString()
+  public override string ToString()
   {
     return Title + string.Empty;
   }
