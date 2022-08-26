ab7cc5cbf7f0aefb8816d299aedaed638bc5bf00
ShadyNagy <info@shadynagy.com>
2022-08-26 22:40:56 +0200

    mapper for book category changed to support  ALUMNI.

--- src/DevBetterWeb.Web/MappingProfiles/BookCategoryProfile.cs
+++ src/DevBetterWeb.Web/MappingProfiles/BookCategoryProfile.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using AutoMapper;
+using DevBetterWeb.Core;
 using DevBetterWeb.Core.Entities;
 using DevBetterWeb.Web.Models;
 
@@ -11,7 +12,9 @@ public class BookCategoryProfile : Profile
   {
     CreateMap<BookCategory, BookCategoryDto>()
 	    .ForPath(dest => dest.Members,
-		    opt => opt.MapFrom(source => source.Books!.Where(b => b.BookCategoryId == source.Id).SelectMany(book => book.MembersWhoHaveRead!).Distinct().ToList()));
+		    opt => opt.MapFrom(source => source.Books!.Where(b => b.BookCategoryId == source.Id).SelectMany(book => book.MembersWhoHaveRead!.Where(m => m.RoleName == AuthConstants.Roles.MEMBERS)).Distinct().ToList()))
+	    .ForPath(dest => dest.Alumnus,
+	    opt => opt.MapFrom(source => source.Books!.Where(b => b.BookCategoryId == source.Id).SelectMany(book => book.MembersWhoHaveRead!.Where(m => m.RoleName == AuthConstants.Roles.ALUMNI)).Distinct().ToList()));
 		CreateMap<BookCategoryDto, BookCategory>();
   }
 }
