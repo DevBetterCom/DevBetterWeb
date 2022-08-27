cc0466fb048040791f199b34a4553091a33782f2
ShadyNagy <info@shadynagy.com>
2022-08-27 19:03:19 +0200

    Stripe card list interface added.

---
+++ src/DevBetterWeb.Infrastructure/Interfaces/IIssuingHandlerCardListService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Stripe.Issuing;
+
+namespace DevBetterWeb.Infrastructure.Interfaces;
+
+public interface IIssuingHandlerCardListService
+{
+	Task<List<Card>> ListAsync(CancellationToken cancellationToken = default);
+	Task<List<Card>> ListByEmailAsync(string memberEmail, CancellationToken cancellationToken = default);
+}
