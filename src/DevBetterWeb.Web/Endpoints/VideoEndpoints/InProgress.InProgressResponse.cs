b922741cddcc304c64c6c0f65e30b084bad343d6
ShadyNagy <info@shadynagy.com>
2022-08-27 22:25:52 +0200

    InProgress removed from videos controller and added as endpoint.

---
+++ src/DevBetterWeb.Web/Endpoints/VideoEndpoints/InProgress.InProgressResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace DevBetterWeb.Web.Endpoints;
+
+public class InProgressResponse
+{
+  public bool Success { get; set; }
+  public string ResponseText { get; set; } = string.Empty;
+
+  public InProgressResponse(bool success, string responseText)
+  {
+	  Success = success;
+		ResponseText = responseText;
+  }
+}
