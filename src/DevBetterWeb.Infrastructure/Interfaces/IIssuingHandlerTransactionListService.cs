82b697fe535d9616c9bb30d029676374d1675feb
ShadyNagy <info@shadynagy.com>
2022-08-27 19:03:35 +0200

    Stripe Transactions list interface added.

---
+++ src/DevBetterWeb.Infrastructure/Interfaces/IIssuingHandlerTransactionListService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Stripe.Issuing;
+
+namespace DevBetterWeb.Infrastructure.Interfaces;
+
+public interface IIssuingHandlerTransactionListService
+{
+	Task<List<Transaction>> ListAsync(CancellationToken cancellationToken = default);
+	Task<List<Transaction>> ListByEmailAsync(string memberEmail, CancellationToken cancellationToken = default);
+}
