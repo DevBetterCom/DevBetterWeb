0f46dd3c6b896e49e8e6d10b256e178c217c3a52
ShadyNagy <info@shadynagy.com>
2022-08-27 19:04:07 +0200

    Stripe issuing card list and Transaction list added.

---
+++ src/DevBetterWeb.Infrastructure/IssuingHandler/StripeIssuingHandler/StripeIssuingHandlerTransactionListService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DevBetterWeb.Infrastructure.Interfaces;
+using Stripe;
+using Stripe.Issuing;
+
+namespace DevBetterWeb.Infrastructure.IssuingHandler.StripeIssuingHandler;
+
+public class StripeIssuingHandlerTransactionListService : IIssuingHandlerTransactionListService
+{
+	private readonly TransactionService _transactionService;
+	private readonly IIssuingHandlerCardListService _issuingHandlerCardListService;
+
+	public StripeIssuingHandlerTransactionListService(TransactionService transactionService, IIssuingHandlerCardListService issuingHandlerCardListService)
+	{
+		_transactionService = transactionService;
+		_issuingHandlerCardListService = issuingHandlerCardListService;
+	}
+
+  public async Task<List<Transaction>> ListAsync(CancellationToken cancellationToken = default)
+  {
+	  var transactionListOptions = new TransactionListOptions { Limit = 100 };
+
+	  var transactions = new List<Transaction>();
+	  var stripeListTransactions = new StripeList<Transaction> { HasMore = true };
+
+	  while (stripeListTransactions.HasMore)
+	  {
+			stripeListTransactions = await _transactionService.ListAsync(transactionListOptions, cancellationToken: cancellationToken);
+			transactions.AddRange(stripeListTransactions.Data);
+		}
+		
+
+		return transactions;
+  }
+
+  public async Task<List<Transaction>> ListByEmailAsync(string memberEmail, CancellationToken cancellationToken = default)
+  {
+		var cards = await _issuingHandlerCardListService.ListByEmailAsync(memberEmail, cancellationToken);
+
+		var transactions = new List<Transaction>();
+		foreach (var card in cards)
+		{
+			var stripeListTransactions = new StripeList<Transaction> { HasMore = true };
+			var transactionListOptions = new TransactionListOptions { Limit = 100, Cardholder = card.Cardholder.Name};
+			while (stripeListTransactions.HasMore)
+			{
+				stripeListTransactions = await _transactionService.ListAsync(transactionListOptions, cancellationToken: cancellationToken);
+				transactions.AddRange(stripeListTransactions.Data);
+			}
+		}
+
+		return transactions;
+  }
+}
