8cfca0d56c5381f5d7d9e07af11c3b3182150dfc
ShadyNagy <info@shadynagy.com>
2022-08-28 00:09:52 +0200

    Filters to check roles and api key added.

---
+++ src/DevBetterWeb.Web/CustomAttributes/UploaderApiOrRoleAuthorization.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevBetterWeb.Web.CustomAttributes;
+
+public class UploaderApiOrRoleAuthorization : TypeFilterAttribute
+{
+	public UploaderApiOrRoleAuthorization(params string[] roles) : base(typeof(UploaderApiAuthorizationFilter))
+	{
+		Arguments = new object[] { roles };
+	}
+}
