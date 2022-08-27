47472cfb1a57c0de0783c7a185b40cabd3c57f95
ShadyNagy <info@shadynagy.com>
2022-08-27 22:51:26 +0200

    Video List Data Table endpoint added.

---
+++ src/DevBetterWeb.Web/Endpoints/VideoEndpoints/VideoListDataTable.VideoListDataTableResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using DevBetterWeb.Web.Models;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+public class VideoListDataTableResponse
+{
+	public string? Draw { get; set; }
+	public decimal CurrentPage { get; set; }
+	public int RecordsFiltered { get; set; }
+	public int RecordsTotal { get; set; }
+	public List<ArchiveVideoDto>? Data { get; set; }
+
+	public VideoListDataTableResponse(string? draw, decimal currentPage, int recordsFiltered, int recordsTotal, List<ArchiveVideoDto>? data)
+	{
+		Draw = draw;
+		CurrentPage = currentPage;
+		RecordsFiltered = recordsFiltered;
+		RecordsTotal = recordsTotal;
+		Data = data;
+	}
+}
