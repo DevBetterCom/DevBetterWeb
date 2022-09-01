2038f0c8c27859596295e362eaceefacd8ca2209
Shady Nagy <info@shadynagy.com>
2022-09-01 14:35:07 +0200

    Shady nagy/admin email reminder cancelations (#890)
    * Full member info added to the SendMemberSubscriptionEndedEmailAsync.
    
    * Call service SendMemberSubscriptionEndedEmailAsync changed to send the member full info.
    
    * Test service SendMemberSubscriptionEndedEmailAsync changed to send null for member full info.
    
    * get member by email.

--- src/DevBetterWeb.Infrastructure/Services/WebhookHandlerService.cs
+++ src/DevBetterWeb.Infrastructure/Services/WebhookHandlerService.cs
@@ -78,7 +78,11 @@ public class WebhookHandlerService : IWebhookHandlerService
 
     await _memberCancellationService.RemoveUserFromMemberRoleAsync(paymentHandlerCustomer.Email);
     await _memberCancellationService.SendCancellationEmailAsync(paymentHandlerCustomer.Email);
-    await _memberSubscriptionEndedAdminEmailService.SendMemberSubscriptionEndedEmailAsync(paymentHandlerCustomer.Email);
+
+    var memberByEmailSpec = new MemberByEmailSpec(paymentHandlerCustomer.Email);
+    var memberFullInfo = await _repository.FirstOrDefaultAsync(memberByEmailSpec);
+
+		await _memberSubscriptionEndedAdminEmailService.SendMemberSubscriptionEndedEmailAsync(paymentHandlerCustomer.Email, memberFullInfo);
     var subscriptionPlanName = _paymentHandlerSubscription.GetAssociatedProductName(paymentHandlerEvent.SubscriptionId);
     var billingPeriod = _paymentHandlerSubscription.GetBillingPeriod(paymentHandlerEvent.SubscriptionId);
     await _memberAddBillingActivityService.AddMemberSubscriptionEndingBillingActivity(paymentHandlerCustomer.Email, subscriptionPlanName, billingPeriod);
