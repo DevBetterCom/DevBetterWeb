fdf9ed8f25ef88c2c98ff319cf03357c9c2107b2
ShadyNagy <shadynagi@gmail.com>
2022-09-07 21:38:52 +0200

    strip invoice service created.

---
+++ src/DevBetterWeb.Infrastructure/Interfaces/IInvoiceHandlerListService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Stripe;
+
+namespace DevBetterWeb.Infrastructure.Interfaces;
+
+public interface IInvoiceHandlerListService
+{
+	Task<List<Invoice>> ListAsync(CancellationToken cancellationToken = default);
+	Task<List<Invoice>> ListByEmailAsync(string memberEmail, CancellationToken cancellationToken = default);
+}
