2c83096228bdd613d16ab2d4bc10af5d011fdd85
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-09-08 19:34:06 +0100

    Pvaillancourt/151 member birthday (#906)
    * Add birthday field to MyProfile
    
    * Add unit tests
    
    * Add validation for february birthday
    
    * Make BirthdayDayAttribute work for every month
    
    * Small refactor
    
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

---
+++ src/DevBetterWeb.Core/ValueObjects/Birthday.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+
+namespace DevBetterWeb.Core.ValueObjects;
+
+public class Birthday : ValueObject
+{
+  public int Day { get; }
+  public int Month { get; }
+  private const int LEAP_YEAR = 2020;
+
+  public Birthday(int day, int month)
+  {
+    new DateOnly(LEAP_YEAR, month, day); // will throw if invalid day/month combination
+    Day = day;
+    Month = month;
+  }
+
+  protected override IEnumerable<object> GetEqualityComponents()
+  {
+    yield return Day;
+    yield return Month;
+  }
+
+  override public string ToString()
+  {
+    return new DateOnly(LEAP_YEAR, Month, Day).ToString("MMMM d");
+  }
+}
---
+++ src/DevBetterWeb.Infrastructure/Data/Migrations/20220907203845_BIRTHDAY.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevBetterWeb.Infrastructure.Data.Migrations
+{
+    public partial class BIRTHDAY : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Birthday_Day",
+                table: "Members",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Birthday_Month",
+                table: "Members",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday_Day",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "Birthday_Month",
+                table: "Members");
+        }
+    }
+}
