7560fa5f3042b03c1218ec02ea6273d940a4a8e5
ShadyNagy <shadynagi@gmail.com>
2022-09-09 06:14:51 +0200

    Create payment intent endpoint added.

---
+++ src/DevBetterWeb.Web/Endpoints/BillingEndpoints/CreatePaymentIntent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Ardalis.ApiEndpoints;
+using DevBetterWeb.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+public class CreatePaymentIntent : EndpointBaseSync
+	.WithRequest<PaymentIntentCreateRequest>
+	.WithActionResult<PaymentIntentCreateResponse>
+{
+	private readonly IPaymentHandlerPrice _paymentHandlerPrice;
+	private readonly IPaymentHandlerPaymentIntent _paymentHandlerPaymentIntent;
+
+	public CreatePaymentIntent(IPaymentHandlerPrice paymentHandlerPrice, IPaymentHandlerPaymentIntent paymentHandlerPaymentIntent)
+	{
+		_paymentHandlerPrice = paymentHandlerPrice;
+		_paymentHandlerPaymentIntent = paymentHandlerPaymentIntent;
+	}
+
+	[HttpPost("create-payment-intent")]
+	public override ActionResult<PaymentIntentCreateResponse> Handle([FromBody] PaymentIntentCreateRequest request)
+	{
+		try
+		{
+			var orderAmount = _paymentHandlerPrice.GetPriceAmount(request.SubscriptionPriceId!);
+			var paymentIntentId = _paymentHandlerPaymentIntent.CreatePaymentIntent(orderAmount);
+			var clientSecret = _paymentHandlerPaymentIntent.GetClientSecret(paymentIntentId);
+
+			return new PaymentIntentCreateResponse(paymentIntentId, clientSecret);
+		}
+		catch (Exception e)
+		{
+			return BadRequest(new PaymentIntentCreateResponse(e.Message)); 
+		}
+	}
+}
