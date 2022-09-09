912db28125d66c06a4e62397aaa391bd899c0c99
ShadyNagy <shadynagi@gmail.com>
2022-09-09 06:14:13 +0200

    Create customer endpoint added. Which needs to be removed if does not use anymore.

---
+++ src/DevBetterWeb.Web/Endpoints/BillingEndpoints/CreateCustomer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ardalis.ApiEndpoints;
+using DevBetterWeb.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+public class CreateCustomer : EndpointBaseSync
+	.WithRequest<CustomerCreateRequest>
+	.WithResult<CustomerCreateResponse>
+{
+	private readonly IPaymentHandlerCustomerService _paymentHandlerCustomer;
+
+	public CreateCustomer(IPaymentHandlerCustomerService paymentHandlerCustomer)
+	{
+		_paymentHandlerCustomer = paymentHandlerCustomer;
+	}
+
+	[HttpPost("create-customer")]
+	public override CustomerCreateResponse Handle([FromBody] CustomerCreateRequest request)
+	{
+		try
+		{
+			var customer = _paymentHandlerCustomer.CreateCustomer(request.Email!);
+
+			return new CustomerCreateResponse(customer.CustomerId, customer.Email);
+		}
+		catch (Exception e)
+		{
+			return new CustomerCreateResponse(e.Message);
+		}
+	}
+}
