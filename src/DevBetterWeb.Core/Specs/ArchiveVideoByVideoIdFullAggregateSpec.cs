a166a4418c47150a923a5dc79e487627a89e3fe7
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-09-13 21:51:49 +0100

    Pvaillancourt/196 video transcripts (#915)
    * Display raw VTT transcript on video details page
    
    * Parrallelize getting the video and the text track
    
    * Refactor Videos/Details/OnGet
    
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

---
+++ src/DevBetterWeb.Core/Specs/ArchiveVideoByVideoIdFullAggregateSpec.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public sealed class ArchiveVideoByVideoIdFullAggregateSpec : Specification<ArchiveVideo>, ISingleResultSpecification
+{
+  public ArchiveVideoByVideoIdFullAggregateSpec(string videoId)
+  {
+    Query
+      .Where(x => x.VideoId == videoId)
+      .Include(X => X.MemberFavorites)
+      .Include(x => x.Comments)
+        .ThenInclude(x => x.MemberWhoCreate)
+      .Include(x => x.Comments)
+        .ThenInclude(x => x.Replies)
+      .Include(x => x.MembersVideoProgress);
+  }
+}
--- src/DevBetterWeb.Web/Pages/Videos/Details.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/Videos/Details.cshtml.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Ardalis.ApiClient;
 using AutoMapper;
 using DevBetterWeb.Core;
 using DevBetterWeb.Core.Entities;
 using DevBetterWeb.Core.Interfaces;
 using DevBetterWeb.Core.Specs;
 using DevBetterWeb.Infrastructure.Identity.Data;
+using DevBetterWeb.Vimeo.Models;
 using DevBetterWeb.Vimeo.Services.VideoServices;
 using DevBetterWeb.Web.Models;
 using DevBetterWeb.Web.Pages.Admin.Videos;
@@ -20,73 +24,137 @@ namespace DevBetterWeb.Web.Pages.Videos;
 [Authorize(Roles = AuthConstants.Roles.ADMINISTRATORS_MEMBERS_ALUMNI)]
 public class DetailsModel : PageModel
 {
-  [BindProperty]
-  public OEmbedViewModel? OEmbedViewModel { get; set; }
+	[BindProperty]
+	public OEmbedViewModel? OEmbedViewModel { get; set; }
+	public string? Transcript { get; set; }
 
-  private readonly GetOEmbedVideoService _getOEmbedVideoService;
-  private readonly GetVideoService _getVideoService;
-  private readonly IRepository<ArchiveVideo> _repository;
-  private readonly IMapper _mapper;
-  private readonly IMarkdownService _markdownService;
-  private readonly UserManager<ApplicationUser> _userManager;
-  private readonly IRepository<Member> _memberRepository;
+	private readonly GetOEmbedVideoService _getOEmbedVideoService;
+	private readonly GetVideoService _getVideoService;
+	private readonly IRepository<ArchiveVideo> _repository;
+	private readonly IMapper _mapper;
+	private readonly IMarkdownService _markdownService;
+	private readonly UserManager<ApplicationUser> _userManager;
+	private readonly IRepository<Member> _memberRepository;
+	private readonly GetAllTextTracksService _getAllTextTracksService;
+	private readonly HttpClient _httpClient;
 
-  public DetailsModel(IMapper mapper, IMarkdownService markdownService, GetOEmbedVideoService getOEmbedVideoService, 
-	  GetVideoService getVideoService, IRepository<ArchiveVideo> repository, UserManager<ApplicationUser> userManager, 
-	  IRepository<Member> memberRepository)
-  {
-	  _mapper = mapper;
-	  _markdownService = markdownService;
-    _getVideoService = getVideoService;
-    _repository = repository;
-    _getOEmbedVideoService = getOEmbedVideoService;
-    _userManager = userManager;
-    _memberRepository = memberRepository;
-  }
+	public DetailsModel(IMapper mapper, IMarkdownService markdownService, GetOEmbedVideoService getOEmbedVideoService,
+		GetVideoService getVideoService, IRepository<ArchiveVideo> repository, UserManager<ApplicationUser> userManager,
+		IRepository<Member> memberRepository, GetAllTextTracksService getAllTextTracksService, HttpClient httpClient)
+	{
+		_mapper = mapper;
+		_markdownService = markdownService;
+		_getVideoService = getVideoService;
+		_repository = repository;
+		_getOEmbedVideoService = getOEmbedVideoService;
+		_userManager = userManager;
+		_memberRepository = memberRepository;
+		_getAllTextTracksService = getAllTextTracksService;
+		_httpClient = httpClient;
+	}
 
-  public async Task<IActionResult> OnGet(string videoId, string? startTime = null)
-  {
-    var video = await _getVideoService.ExecuteAsync(videoId);
-    if (video?.Data == null) return NotFound($"Video Not Found {videoId}");
+	public async Task<IActionResult> OnGet(string videoId, string? startTime = null)
+	{
+		var (video, textTracks, archiveVideo, applicationUser) = await GetDataAsync(videoId);
+		if (video?.Data == null) return NotFound($"Video Not Found {videoId}");
+		if (archiveVideo == null) return NotFound($"Video Not Found {videoId}");
 
-    var oEmbed = await _getOEmbedVideoService.ExecuteAsync(video.Data.Link);
+    var (oEmbed, member) = await GetMoreDataAsync(video.Data.Link, applicationUser.Id);
     if (oEmbed?.Data == null) return NotFound($"Video Not Found {videoId}");
-
-    var videoSpec = new ArchiveVideoByVideoIdWithMemberFavoritesAndCommentsSpec(videoId);
-    var archiveVideo = await _repository.FirstOrDefaultAsync(videoSpec);
-    if (archiveVideo == null) return NotFound($"Video Not Found {videoId}");
-
-    archiveVideo.Views++;
-    await _repository.UpdateAsync(archiveVideo);
-
-    var currentUserName = User.Identity!.Name;
-    var applicationUser = await _userManager.FindByNameAsync(currentUserName);
-
-    var memberSpec = new MemberByUserIdWithFavoriteArchiveVideosSpec(applicationUser.Id);
-    var member = await _memberRepository.FirstOrDefaultAsync(memberSpec);
     if (member == null) return NotFound($"Member Not Found {applicationUser.Id}");
 
-    var spec = new ArchiveVideoByVideoIdWithProgressSpec(videoId);
-    var existVideoWithProgress = await _repository.FirstOrDefaultAsync(spec);
-    var progress = existVideoWithProgress!.MembersVideoProgress.FirstOrDefault(x => x.ArchiveVideoId == existVideoWithProgress.Id && x.MemberId == member.Id);
+		if (textTracks?.Data != null)
+		{
+			await GetTranscript(textTracks);
+		}		
 
-		OEmbedViewModel = new OEmbedViewModel(oEmbed.Data);
-    OEmbedViewModel.VideoId = int.Parse(archiveVideo.VideoId!);
-    OEmbedViewModel.Name = archiveVideo.Title;
+		BuildOEmbedViewModel(startTime, video.Data, oEmbed.Data, archiveVideo, member);
 
-    archiveVideo.CreateMdComments(_markdownService);
+		archiveVideo.Views++;
+		await _repository.UpdateAsync(archiveVideo);
+
+		return Page();
+	}
+
+	private async Task<(HttpResponse<Video>, HttpResponse<GetAllTextTracksResponse>, ArchiveVideo?, ApplicationUser)> GetDataAsync(string videoId)
+	{
+		var videoTask = _getVideoService.ExecuteAsync(videoId);
+		var textTracksTask = _getAllTextTracksService.ExecuteAsync(videoId);
+		var videoSpec = new ArchiveVideoByVideoIdFullAggregateSpec(videoId);
+		var archiveVideoTask = _repository.FirstOrDefaultAsync(videoSpec);
+		var currentUserName = User.Identity!.Name;
+		var applicationUserTask = _userManager.FindByNameAsync(currentUserName);
+
+		var task = Task.WhenAll(videoTask, textTracksTask, archiveVideoTask, applicationUserTask);
+		try
+		{
+			await task;
+		}
+		catch (Exception)
+		{
+			if (task.Exception != null)
+			{
+				throw task.Exception;
+			}
+
+			throw;
+		}
+
+		return (videoTask.Result, textTracksTask.Result, archiveVideoTask.Result, applicationUserTask.Result);
+	}
+
+  private async Task<(HttpResponse<OEmbed>, Member?)> GetMoreDataAsync(string videoLink, string userId)
+  {
+    var oEmbedTask = _getOEmbedVideoService.ExecuteAsync(videoLink);
+    var memberSpec = new MemberByUserIdWithFavoriteArchiveVideosSpec(userId);
+    var memberTask = _memberRepository.FirstOrDefaultAsync(memberSpec);
+
+    var task = Task.WhenAll(oEmbedTask, memberTask);
+		try
+		{
+			await task;
+		}
+		catch (Exception)
+		{
+			if (task.Exception != null)
+			{
+				throw task.Exception;
+			}
+
+			throw;
+		}
+
+		return (oEmbedTask.Result, memberTask.Result);
+  }
+
+	private async Task GetTranscript(HttpResponse<GetAllTextTracksResponse> textTracks)
+	{
+		var textTrackUrl = textTracks.Data.Data.First().Link;
+		var textTrackResponse = (await _httpClient.GetAsync(textTrackUrl));
+		if (textTrackResponse.IsSuccessStatusCode)
+		{
+			Transcript = await textTrackResponse.Content.ReadAsStringAsync();
+		}
+	}
+
+	private void BuildOEmbedViewModel(string? startTime, Video video, OEmbed oEmbed, ArchiveVideo archiveVideo, Member member)
+	{
+		var progress = archiveVideo.MembersVideoProgress.FirstOrDefault(x => x.ArchiveVideoId == archiveVideo.Id && x.MemberId == member.Id);
+		OEmbedViewModel = new OEmbedViewModel(oEmbed);
+		OEmbedViewModel.VideoId = int.Parse(archiveVideo.VideoId!);
+		OEmbedViewModel.Name = archiveVideo.Title;
+
+		archiveVideo.CreateMdComments(_markdownService);
 		OEmbedViewModel.Comments = _mapper.Map<List<VideoCommentDto>>(archiveVideo.Comments);
 
-    OEmbedViewModel.Password = video.Data.Password;
-    OEmbedViewModel.DescriptionMd = _markdownService.RenderHTMLFromMD(archiveVideo.Description);
-    OEmbedViewModel.Description = archiveVideo.Description;
-    OEmbedViewModel
-      .AddStartTime(startTime)
-      .BuildHtml(video.Data.Link);
-    OEmbedViewModel.IsMemberFavorite = member.FavoriteArchiveVideos.Any(fav => fav.ArchiveVideoId == archiveVideo.Id);
-    OEmbedViewModel.IsMemberWatched = progress is { VideoWatchedStatus: VideoWatchedStatus.Watched };
+		OEmbedViewModel.Password = video.Password;
+		OEmbedViewModel.DescriptionMd = _markdownService.RenderHTMLFromMD(archiveVideo.Description);
+		OEmbedViewModel.Description = archiveVideo.Description;
+		OEmbedViewModel
+			.AddStartTime(startTime)
+			.BuildHtml(video.Link);
+		OEmbedViewModel.IsMemberFavorite = member.FavoriteArchiveVideos.Any(fav => fav.ArchiveVideoId == archiveVideo.Id);
+		OEmbedViewModel.IsMemberWatched = progress is { VideoWatchedStatus: VideoWatchedStatus.Watched };
 		OEmbedViewModel.MemberFavoritesCount = archiveVideo.MemberFavorites.Count();
-
-    return Page();
-  }
+	}
 }
