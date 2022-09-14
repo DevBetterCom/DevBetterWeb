1acdefcb61ed57221d5046bc5570c0cee9bdbb15
rostech <rostislavkazarin@gmail.com>
2022-09-14 23:40:40 +0300

    New form for  shipping address is added to profile page

---
+++ src/DevBetterWeb.Web/Pages/User/UserShippingAddressUpdateModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Web.Pages.User;
+
+public class UserShippingAddressUpdateModel
+{
+	public UserShippingAddressUpdateModel()
+	{
+	}
+
+	public UserShippingAddressUpdateModel(Member member)
+	{
+		this.Street = member.ShippingAddress.Street;
+		this.City = member.ShippingAddress.City;
+		this.State = member.ShippingAddress.State;
+		this.PostalCode = member.ShippingAddress.PostalCode;
+		this.Country = member.ShippingAddress.Country;
+	}
+
+	[Required]
+	public string City { get; set; }
+
+	[Required]
+	public string Country { get; set; }
+
+	public string State { get; set; }
+	public string Street { get; set; }
+
+	public string PostalCode { get; set; }
+
+}
