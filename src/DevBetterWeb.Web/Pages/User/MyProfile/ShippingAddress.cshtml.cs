2bec08d739f45dceaa8151c50f1f8310f73ab51c
rostech <rostislavkazarin@gmail.com>
2022-09-16 12:00:35 +0300

    Suppressed non-nullable warnings related to shipping address

--- src/DevBetterWeb.Web/Pages/User/MyProfile/ShippingAddress.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/User/MyProfile/ShippingAddress.cshtml.cs
@@ -20,7 +20,9 @@ namespace DevBetterWeb.Web.Pages.User
 		private readonly IMemberRegistrationService _memberRegistrationService;
 		private readonly IRepository<Member> _memberRepository;
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 		public ShippingAddressModel(UserManager<ApplicationUser> userManager,
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 			IMemberRegistrationService memberRegistrationService,
 			IRepository<Member> memberRepository)
 		{
@@ -65,7 +67,9 @@ namespace DevBetterWeb.Web.Pages.User
 				UserShippingAddressUpdateModel.PostalCode,
 				UserShippingAddressUpdateModel.Country));
 
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
 			member.UpdateAddress(member.ShippingAddress.ToString());
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 
 			await _memberRepository.UpdateAsync(member);
 		}
--- src/DevBetterWeb.Web/Pages/User/UserShippingAddressUpdateModel.cs
+++ src/DevBetterWeb.Web/Pages/User/UserShippingAddressUpdateModel.cs
@@ -5,13 +5,17 @@ namespace DevBetterWeb.Web.Pages.User;
 
 public class UserShippingAddressUpdateModel
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 	public UserShippingAddressUpdateModel()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 	{
 	}
 
 	public UserShippingAddressUpdateModel(Member member)
 	{
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
 		this.Street = member.ShippingAddress.Street;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 		this.City = member.ShippingAddress.City;
 		this.State = member.ShippingAddress.State;
 		this.PostalCode = member.ShippingAddress.PostalCode;
