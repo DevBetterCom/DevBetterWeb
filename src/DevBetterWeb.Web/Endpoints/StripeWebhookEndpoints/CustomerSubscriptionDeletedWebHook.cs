5865c1a7b0125c801dc73c8eee9e2fc0ff0bc974
ShadyNagy <shadynagi@gmail.com>
2022-09-18 20:35:55 +0200

    logger added on CustomerSubscriptionDeletedWebHook.

--- src/DevBetterWeb.Web/Endpoints/StripeWebhookEndpoints/CustomerSubscriptionDeletedWebHook.cs
+++ src/DevBetterWeb.Web/Endpoints/StripeWebhookEndpoints/CustomerSubscriptionDeletedWebHook.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Ardalis.ApiEndpoints;
 using Ardalis.GuardClauses;
 using DevBetterWeb.Core.Interfaces;
-using DevBetterWeb.Infrastructure.PaymentHandler;
 using DevBetterWeb.Infrastructure.Services;
+using DevBetterWeb.Web.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Stripe;
@@ -17,15 +17,18 @@ public class CustomerSubscriptionDeletedWebHook : EndpointBaseAsync
 	.WithoutRequest
 	.WithActionResult
 {
+	private readonly IAppLogger<CustomerSubscriptionDeletedWebHook> _logger;
 	private readonly IWebhookHandlerService _webHookHandlerService;
 	private readonly string _stripeWebHookSecretKey;
 	public CustomerSubscriptionDeletedWebHook(
+		IAppLogger<CustomerSubscriptionDeletedWebHook> logger,
 		IOptions<StripeOptions> optionsAccessor,
 		IWebhookHandlerService webHookHandlerService)
 	{
 		Guard.Against.Null(optionsAccessor, nameof(optionsAccessor));
 		Guard.Against.Null(optionsAccessor.Value?.StripeWebHookSecretKey, nameof(optionsAccessor.Value.StripeWebHookSecretKey));
 
+		_logger = logger;
 		_webHookHandlerService = webHookHandlerService;
 		_stripeWebHookSecretKey = optionsAccessor.Value.StripeWebHookSecretKey;
 	}
@@ -33,12 +36,16 @@ public class CustomerSubscriptionDeletedWebHook : EndpointBaseAsync
 	[HttpPost("stripe-customer-subscription-deleted-web-hook")]
 	public override async Task<ActionResult> HandleAsync(CancellationToken cancellationToken = default)
 	{
+		_logger.LogInformation("Start Stripe Endpoint: stripe-customer-subscription-deleted-web-hook");
+
+		var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
 		try
 		{
-			var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-
 			var stripeEvent = EventUtility.ConstructEvent(json,
 				Request.Headers["Stripe-Signature"], _stripeWebHookSecretKey);
+
+			_logger.LogInformation($"Processing Stripe Event Type: {stripeEvent.Type}");
 
 			if (stripeEvent.Type != Events.CustomerDeleted)
 			{
@@ -51,6 +58,7 @@ public class CustomerSubscriptionDeletedWebHook : EndpointBaseAsync
 		}
 		catch (StripeException exception)
 		{
+			_logger.LogError(exception, "Stripe callback error", json);
 			return BadRequest(exception.Message);
 		}
 	}
