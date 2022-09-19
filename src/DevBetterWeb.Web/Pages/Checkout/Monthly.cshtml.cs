657d2571c82c31d6f2e2d675cb75dfe9407fb641
ShadyNagy <shadynagi@gmail.com>
2022-09-19 02:59:14 +0200

    Rename stripe options.

--- src/DevBetterWeb.Web/Pages/Checkout/Monthly.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/Checkout/Monthly.cshtml.cs
@@ -14,9 +14,9 @@ public class MonthlyModel : PageModel
   public MonthlyModel(IOptions<StripeOptions> optionsAccessor)
   {
     Guard.Against.Null(optionsAccessor, nameof(optionsAccessor));
-    Guard.Against.NullOrEmpty(optionsAccessor.Value.stripePublishableKey, nameof(optionsAccessor.Value.stripePublishableKey));
+    Guard.Against.NullOrEmpty(optionsAccessor.Value.StripePublishableKey, nameof(optionsAccessor.Value.StripePublishableKey));
     SubscriptionType = new SubscriptionTypeViewModel("Monthly", "month", 200,
-      optionsAccessor.Value.stripePublishableKey, optionsAccessor.Value.monthlyPlanId!);
+      optionsAccessor.Value.StripePublishableKey, optionsAccessor.Value.MonthlyPlanId!);
   }
 
   public void OnGet()
