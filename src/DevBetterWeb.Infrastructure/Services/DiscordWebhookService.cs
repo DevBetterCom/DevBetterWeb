3143d3fb9523fcf4b6863059a64736d47b96ed93
ShadyNagy <shadynagi@gmail.com>
2023-03-10 21:04:14 +0200

    Discord Models are added. Discord service is created.

---
+++ src/DevBetterWeb.Infrastructure/Services/DiscordWebhookService.cs
@@ -0,0 +1,46 @@
+﻿using DevBetterWeb.Infrastructure.DiscordWebhooks;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.Interfaces;
+
+namespace DevBetterWeb.Infrastructure.Services;
+
+public class DiscordWebhookService : IDiscordWebhookService
+{
+	private readonly HttpClient _httpClient = new HttpClient();
+	private readonly IAppLogger<DiscordWebhookService> _logger;
+
+	public DiscordWebhookService(IAppLogger<DiscordWebhookService> logger)
+	{
+		_logger = logger;
+	}
+
+	public Task<HttpResponseMessage> SendAsync(ulong id, string token, string contentBody, string? username = null, string? avatarUrl = null, bool isTTS = false, IEnumerable<Embed>? embeds = null)
+	{
+		var webhookUrl = $"https://discordapp.com/api/webhooks/{id}/{token}";
+
+		return SendAsync(webhookUrl, contentBody, username, avatarUrl, isTTS, embeds);
+	}
+
+	public async Task<HttpResponseMessage> SendAsync(string webhookUrl, string contentBody, string? username = null, string? avatarUrl = null, bool isTTS = false, IEnumerable<Embed>? embeds = null)
+	{
+		var request = new WebHookRequest(contentBody, username, avatarUrl, isTTS, embeds);
+		var json = JsonSerializer.Serialize(request);
+
+		var content = new StringContent(json, Encoding.UTF8, "application/json");
+		try
+		{
+			return await _httpClient.PostAsync(webhookUrl, content);
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError(ex, "Error sending webhook", webhookUrl, content);
+			return new HttpResponseMessage();
+		}
+	}
+}
