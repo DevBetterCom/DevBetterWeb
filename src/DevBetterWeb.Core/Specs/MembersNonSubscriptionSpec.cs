263c5d04671e110d7213ce4c712b44b19e4223fb
Shady Nagy <info@shadynagy.com>
2023-05-08 18:19:51 +0300

    Enhance Leaderboard Filtering by Managing Non-Current Members (#1088)
    * LeaderBoardService is created and can get non currently members.
    
    * The NonCurrentMembersService is designed to retrieve members who are not considered current members.
    
    * Filtered Leaderboard Service is created.
    
    * new service is working now.
    
    * unit test is added.

---
+++ src/DevBetterWeb.Core/Specs/MembersNonSubscriptionSpec.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public sealed class MembersNonSubscriptionSpec : Specification<Member>
+{
+  public MembersNonSubscriptionSpec(List<string> usersIdWithoutMemberRole)
+  {
+    Query
+      .Where(member => usersIdWithoutMemberRole.Contains(member.UserId));
+  }
+}
+
---
+++ src/DevBetterWeb.Web/Services/FilteredLeaderboardService.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DevBetterWeb.Web.Models;
+using System.Linq;
+using DevBetterWeb.Web.Interfaces;
+using DevBetterWeb.Infrastructure.Interfaces;
+
+namespace DevBetterWeb.Web.Services;
+
+/// <summary>
+/// Provides functionality to manage leaderboards with filtered members.
+/// </summary>
+public class FilteredLeaderboardService : IFilteredLeaderboardService
+{
+	private readonly INonCurrentMembersService _nonCurrentMembersService;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FilteredLeaderboardService"/> class.
+	/// </summary>
+	/// <param name="nonCurrentMembersService">The service for managing non-current members.</param>
+	public FilteredLeaderboardService(INonCurrentMembersService nonCurrentMembersService)
+	{
+		_nonCurrentMembersService = nonCurrentMembersService;
+	}
+
+	/// <summary>
+	/// Removes non-current members from the leaderboard.
+	/// </summary>
+	/// <param name="bookCategories">The list of book categories in the leaderboard.</param>
+	/// <param name="cancellationToken">An optional token to cancel the operation.</param>
+	/// <returns>A new list of book categories with non-current members removed.</returns>
+	public async Task<List<BookCategoryDto>> RemoveNonCurrentMembersFromLeaderBoardAsync(List<BookCategoryDto> bookCategories, CancellationToken cancellationToken = default)
+	{
+		var nonUsersId = await _nonCurrentMembersService.GetUsersIdsWithoutRolesAsync();
+		var nonMembersId = await _nonCurrentMembersService.GetNonCurrentMembersAsync(nonUsersId, cancellationToken);
+
+		return bookCategories
+				.Select(bookCategory => CreateBookCategoryDtoWithoutNonMembers(bookCategory, nonMembersId))
+				.ToList();
+	}
+
+	private BookCategoryDto CreateBookCategoryDtoWithoutNonMembers(BookCategoryDto bookCategory, List<int> nonMembersId)
+	{
+		var filteredBooks = CreateBookDtosWithoutNonMembers(bookCategory.Books!, nonMembersId);
+
+		var membersFromBooks = filteredBooks
+			.SelectMany(book => book.MembersWhoHaveRead)
+			.Distinct()
+			.ToList();
+
+		var bookCategoryToAdd = new BookCategoryDto
+		{
+			Id = bookCategory.Id,
+			Title = bookCategory.Title,
+			Members = membersFromBooks,
+			Books = filteredBooks
+		};
+
+		return bookCategoryToAdd;
+	}
+
+	private List<MemberForBookDto> RemoveNonMembersFromList(List<MemberForBookDto> members, List<int> nonMembersId)
+	{
+		return members
+				.Where(member => !nonMembersId.Contains(member.Id))
+				.ToList();
+	}
+
+	private List<BookDto> CreateBookDtosWithoutNonMembers(List<BookDto> books, List<int> nonMembersId)
+	{
+		return books
+				.Select(book => CreateBookDtoWithoutNonMembers(book, nonMembersId))
+				.ToList();
+	}
+
+	private BookDto CreateBookDtoWithoutNonMembers(BookDto book, List<int> nonMembersId)
+	{
+		var bookToAdd = new BookDto
+		{
+			Id = book.Id,
+			Title = book.Title,
+			Author = book.Author,
+			BookCategoryId = book.BookCategoryId,
+			CategoryTitle = book.CategoryTitle,
+			Details = book.Details,
+			MemberWhoUploaded = book.MemberWhoUploaded,
+			MemberWhoUploadedUserId = book.MemberWhoUploadedUserId,
+			PurchaseUrl = book.PurchaseUrl,
+			TitleWithAuthor = book.TitleWithAuthor,
+			MembersWhoHaveRead = RemoveNonMembersFromList(book.MembersWhoHaveRead, nonMembersId)
+		};
+
+		return bookToAdd;
+	}
+}
