ed16fc3b9b6f4d07a917c8902ec942e295f5b745
ShadyNagy <shadynagi@gmail.com>
2023-05-09 06:40:13 +0300

    book category service is added.

---
+++ src/DevBetterWeb.Web/Interfaces/IBookCategoryService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevBetterWeb.Web.Models;
+
+namespace DevBetterWeb.Web.Interfaces;
+
+public interface IBookCategoryService
+{
+	Task<List<BookCategoryDto>> GetBookCategoriesAsync();
+}
---
+++ src/DevBetterWeb.Web/Services/BookCategoryService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using DevBetterWeb.Core.Entities;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Core.Specs;
+using DevBetterWeb.Web.Interfaces;
+using DevBetterWeb.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevBetterWeb.Web.Services;
+
+public class BookCategoryService : IBookCategoryService
+{
+	private readonly IMapper _mapper;
+	private readonly IRepository<BookCategory> _bookCategoryRepository;
+
+	public BookCategoryService(IMapper mapper, IRepository<BookCategory> bookCategoryRepository)
+	{
+		_mapper = mapper;
+		_bookCategoryRepository = bookCategoryRepository;
+	}
+
+	public async Task<List<BookCategoryDto>> GetBookCategoriesAsync()
+	{
+		var spec = new BookCategoriesSpec();
+		var bookCategoriesEntity = await _bookCategoryRepository.ListAsync(spec);
+		return _mapper.Map<List<BookCategoryDto>>(bookCategoriesEntity);
+	}
+}
