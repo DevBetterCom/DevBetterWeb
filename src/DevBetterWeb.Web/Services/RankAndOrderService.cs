ec3c98976f1ebf41a28891f3bb0ce017dd67bc02
ShadyNagy <shadynagi@gmail.com>
2023-05-09 20:48:00 +0300

    comments are added for RankAndOrderService and IRankAndOrderService.

--- src/DevBetterWeb.Web/Services/RankAndOrderService.cs
+++ src/DevBetterWeb.Web/Services/RankAndOrderService.cs
@@ -6,123 +6,141 @@ using DevBetterWeb.Core.Entities;
 using DevBetterWeb.Web.Interfaces;
 using DevBetterWeb.Web.Models;
 
-namespace DevBetterWeb.Web.Services
+namespace DevBetterWeb.Web.Services;
+
+/// <summary>
+/// Service for ranking and ordering members and books.
+/// </summary>
+public class RankAndOrderService : IRankAndOrderService
 {
-	public class RankAndOrderService : IRankAndOrderService
+	private readonly IRankingService _rankingService;
+	private readonly IMemberService _memberService;
+
+	public RankAndOrderService(IRankingService rankingService, IMemberService memberService)
 	{
-		private readonly IRankingService _rankingService;
-		private readonly IMemberService _memberService;
+		_rankingService = rankingService;
+		_memberService = memberService;
+	}
 
-		public RankAndOrderService(IRankingService rankingService, IMemberService memberService)
+	/// <summary>
+	/// Asynchronously updates ranks and the count of books read for each member.
+	/// </summary>
+	/// <param name="bookCategories">The list of book categories.</param>
+	public async Task UpdateRanksAndReadBooksCountForMemberAsync(List<BookCategoryDto> bookCategories)
+	{
+		foreach (var bookCategory in bookCategories)
 		{
-			_rankingService = rankingService;
-			_memberService = memberService;
+			await UpdateRanksAndReadBooksCountForMemberAsync(bookCategory);
 		}
+	}
 
-		public async Task UpdateRanksAndReadBooksCountForMemberAsync(List<BookCategoryDto> bookCategories)
+	/// <summary>
+	/// Updates the reading ranks of members.
+	/// </summary>
+	/// <param name="bookCategories">The list of book categories.</param>
+	public void UpdateMembersReadRank(List<BookCategoryDto> bookCategories)
+	{
+		if (bookCategories.Count <= 0)
 		{
-			foreach (var bookCategory in bookCategories)
+			return;
+		}
+		foreach (var category in bookCategories)
+		{
+			if (category.Books.Count <= 0)
 			{
-				await UpdateRanksAndReadBooksCountForMemberAsync(bookCategory);
+				continue;
 			}
+			_rankingService.CalculateMemberRank(category.Members);
+			_rankingService.CalculateMemberRank(category.Alumnus);
 		}
+	}
 
-		public void UpdateMembersReadRank(List<BookCategoryDto> bookCategories)
+	/// <summary>
+	/// Updates the ranks of books.
+	/// </summary>
+	/// <param name="bookCategories">The list of book categories.</param>
+	public void UpdateBooksRank(List<BookCategoryDto> bookCategories)
+	{
+		if (bookCategories.Count <= 0)
 		{
-			if (bookCategories.Count <= 0)
+			return;
+		}
+		foreach (var category in bookCategories)
+		{
+			if (category.Books.Count <= 0)
+			{
+				continue;
+			}
+			_rankingService.CalculateBookRank(category.Books!);
+		}
+	}
+
+	/// <summary>
+	/// Orders members and books by rank.
+	/// </summary>
+	/// <param name="bookCategories">The list of book categories.</param>
+	public void OrderByRankForMembersAndBooks(List<BookCategoryDto> bookCategories)
+	{
+		foreach (var bookCategory in bookCategories)
+		{
+			bookCategory.Members = _rankingService.OrderMembersByRank(bookCategory.Members);
+			bookCategory.Alumnus = _rankingService.OrderMembersByRank(bookCategory.Alumnus);
+			bookCategory.Books = _rankingService.OrderBooksByRank(bookCategory.Books!);
+		}
+	}
+
+	private async Task UpdateRanksAndReadBooksCountForMemberAsync(BookDto book, BookCategoryDto bookCategory)
+	{
+		foreach (var memberWhoHaveRead in book.MembersWhoHaveRead!)
+		{
+			await UpdateRanksAndReadBooksCountForMemberAsync(memberWhoHaveRead, bookCategory);
+			book.MembersWhoHaveReadCount = book.MembersWhoHaveRead.Count;
+		}
+	}
+
+	private async Task UpdateRanksAndReadBooksCountForMemberAsync(BookCategoryDto bookCategory)
+	{
+		foreach (var book in bookCategory.Books!)
+		{
+			await UpdateRanksAndReadBooksCountForMemberAsync(book, bookCategory);
+		}
+	}
+
+	private async Task UpdateRanksAndReadBooksCountForMemberAsync(MemberForBookDto memberWhoHaveRead, BookCategoryDto bookCategory)
+	{
+		var memberToAdd = new MemberForBookDto
+		{
+			Id = memberWhoHaveRead.Id,
+			FullName = memberWhoHaveRead.FullName,
+			BooksReadCount = memberWhoHaveRead.BooksRead!.Count(x => x.BookCategoryId == bookCategory.Id),
+			UserId = memberWhoHaveRead.UserId,
+		};
+
+		List<Member> alumniMembers = await _memberService.GetActiveAlumniMembersAsync();
+		List<int> alumniMemberIds = alumniMembers.Select(x => x.Id).ToList();
+
+		var isAlumni = alumniMemberIds.Contains(memberWhoHaveRead.Id);
+		if (isAlumni)
+		{
+			if (bookCategory.Alumnus.Any(m => m.Id == memberWhoHaveRead.Id))
 			{
 				return;
 			}
-			foreach (var category in bookCategories)
-			{
-				if (category.Books.Count <= 0)
-				{
-					continue;
-				}
-				_rankingService.CalculateMemberRank(category.Members);
-				_rankingService.CalculateMemberRank(category.Alumnus);
-			}
-		}
 
-		public void UpdateBooksRank(List<BookCategoryDto> bookCategories)
+			memberToAdd.RoleName = AuthConstants.Roles.ALUMNI;
+			bookCategory.Alumnus.Add(memberToAdd);
+		}
+		else
 		{
-			if (bookCategories.Count <= 0)
+			var member = bookCategory.Members.FirstOrDefault(m => m.Id == memberWhoHaveRead.Id);
+			if (member != null)
 			{
+				member.RoleName = AuthConstants.Roles.MEMBERS;
 				return;
 			}
-			foreach (var category in bookCategories)
-			{
-				if (category.Books.Count <= 0)
-				{
-					continue;
-				}
-				_rankingService.CalculateBookRank(category.Books!);
-			}
-		}
 
-		public void OrderByRankForMembersAndBooks(List<BookCategoryDto> bookCategories)
-		{
-			foreach (var bookCategory in bookCategories)
-			{
-				bookCategory.Members = _rankingService.OrderMembersByRank(bookCategory.Members);
-				bookCategory.Alumnus = _rankingService.OrderMembersByRank(bookCategory.Alumnus);
-				bookCategory.Books = _rankingService.OrderBooksByRank(bookCategory.Books!);
-			}
-		}
-
-		private async Task UpdateRanksAndReadBooksCountForMemberAsync(BookDto book, BookCategoryDto bookCategory)
-		{
-			foreach (var memberWhoHaveRead in book.MembersWhoHaveRead!)
-			{
-				await UpdateRanksAndReadBooksCountForMemberAsync(memberWhoHaveRead, bookCategory);
-				book.MembersWhoHaveReadCount = book.MembersWhoHaveRead.Count;
-			}
-		}
-
-		private async Task UpdateRanksAndReadBooksCountForMemberAsync(BookCategoryDto bookCategory)
-		{
-			foreach (var book in bookCategory.Books!)
-			{
-				await UpdateRanksAndReadBooksCountForMemberAsync(book, bookCategory);
-			}
-		}
-
-		private async Task UpdateRanksAndReadBooksCountForMemberAsync(MemberForBookDto memberWhoHaveRead, BookCategoryDto bookCategory)
-		{
-			var memberToAdd = new MemberForBookDto
-			{
-				Id = memberWhoHaveRead.Id,
-				FullName = memberWhoHaveRead.FullName,
-				BooksReadCount = memberWhoHaveRead.BooksRead!.Count(x => x.BookCategoryId == bookCategory.Id),
-				UserId = memberWhoHaveRead.UserId,
-			};
-
-			List<Member> alumniMembers = await _memberService.GetActiveAlumniMembersAsync();
-			List<int> alumniMemberIds = alumniMembers.Select(x => x.Id).ToList();
-
-			var isAlumni = alumniMemberIds.Contains(memberWhoHaveRead.Id);
-			if (isAlumni)
-			{
-				if (bookCategory.Alumnus.Any(m => m.Id == memberWhoHaveRead.Id))
-				{
-					return;
-				}
-
-				memberToAdd.RoleName = AuthConstants.Roles.ALUMNI;
-				bookCategory.Alumnus.Add(memberToAdd);
-			}
-			else
-			{
-				var member = bookCategory.Members.FirstOrDefault(m => m.Id == memberWhoHaveRead.Id);
-				if (member != null)
-				{
-					member.RoleName = AuthConstants.Roles.MEMBERS;
-					return;
-				}
-
-				memberToAdd.RoleName = AuthConstants.Roles.MEMBERS;
-				bookCategory.Members.Add(memberToAdd);
-			}
+			memberToAdd.RoleName = AuthConstants.Roles.MEMBERS;
+			bookCategory.Members.Add(memberToAdd);
 		}
 	}
 }
