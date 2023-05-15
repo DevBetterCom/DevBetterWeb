6809ddba3abb262ecad8428f7048c2428c457ff8
ShadyNagy <shadynagi@gmail.com>
2023-05-15 15:39:48 +0300

    book page created.

---
+++ src/DevBetterWeb.Core/Specs/BooksOrderedByReadsSpec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using DevBetterWeb.Core.Entities;
+
+namespace DevBetterWeb.Core.Specs;
+
+public sealed class BooksOrderedByReadsSpec : Specification<Book>
+{
+  public BooksOrderedByReadsSpec()
+  {
+		Query
+			.Include(book => book.MemberWhoUpload)
+			.Include(book => book.MembersWhoHaveRead)
+			.Include(book => book.BookCategory);
+  }
+}
---
+++ src/DevBetterWeb.Web/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevBetterWeb.Core;
+using DevBetterWeb.Web.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevBetterWeb.Web.Pages.Books;
+
+[Authorize(Roles = AuthConstants.Roles.ADMINISTRATORS_MEMBERS_ALUMNI)]
+public class IndexModel : PageModel
+{
+	private readonly IBookService _bookService;
+
+  public List<BookViewModel> Books { get; set; } = new ();
+
+  public IndexModel(IBookService bookService)
+  {
+		_bookService = bookService;
+  }
+
+  public async Task OnGet()
+  {
+		Books = await _bookService.GetAllBooksAsync();
+  }
+}
