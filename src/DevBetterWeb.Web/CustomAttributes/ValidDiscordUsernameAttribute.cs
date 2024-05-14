0fee4fe43cf435ded461d6fc153e683731db1080
Cesar Aguirre <canro91@hotmail.com>
2024-05-14 11:23:40 -0500

    Change Discord username validation to allow digits, underscore and period (#1294)

--- src/DevBetterWeb.Web/CustomAttributes/ValidDiscordUsernameAttribute.cs
+++ src/DevBetterWeb.Web/CustomAttributes/ValidDiscordUsernameAttribute.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace DevBetterWeb.Web;
 
 public class ValidDiscordUsernameAttribute : ValidationAttribute
 {
   public string GetErrorMessage() =>
-  $"You must enter a valid Discord username with 4-digit tag (username#1234)";
+  $"You must enter a valid Discord username with between 2 and 32 digits. Only Latin characters, numbers, underscore, and period";
 
 #pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
   protected override ValidationResult IsValid(object value,
@@ -24,15 +25,9 @@ public class ValidDiscordUsernameAttribute : ValidationAttribute
       return ValidationResult.Success!;
     }
 
-    if (username.Length > 6)
-    {
-
-      if (ValidDiscordUsernameAttributeHelpers.HasHashtagFourFromEnd(username) && ValidDiscordUsernameAttributeHelpers.HasFourDigitNumAtEnd(username))
-      {
-        return ValidationResult.Success!;
-      }
-    }
-
-    return new ValidationResult(GetErrorMessage());
-  }
+		return Regex.IsMatch(username, "^[a-z0-9._]{2,32}$")
+								&& !username.Contains("..")
+						? ValidationResult.Success!
+						: new ValidationResult(GetErrorMessage());
+	}
 }
---
+++ tests/DevBetterWeb.Tests/Integration/Web/CustomAttributes/ValidDiscordUsernameAttributeShould.cs
@@ -0,0 +1,81 @@
+﻿using DevBetterWeb.Web;
+using Xunit;
+
+namespace DevBetterWeb.Tests.Web.CustomAttributes;
+
+public class ValidDiscordUsernameAttributeShould
+{
+	[Fact]
+	public void GiveFalseIfHas1Character()
+	{
+		var attribute = new ValidDiscordUsernameAttribute();
+		
+		var input = "a";
+		var isValid = attribute.IsValid(input);
+
+		Assert.False(isValid);
+	}
+
+	[Fact]
+	public void GiveFalseIfHasMoreThan32Characters()
+	{
+		var attribute = new ValidDiscordUsernameAttribute();
+
+		var input = new string('a', 33);
+		var isValid = attribute.IsValid(input);
+
+		Assert.False(isValid);
+	}
+
+	[Fact]
+	public void GiveFalseIfHasSpecialCharacterBesidesUnderscoreAndPeriod()
+	{
+		var attribute = new ValidDiscordUsernameAttribute();
+
+		var input = "john!@#$";
+		var isValid = attribute.IsValid(input);
+
+		Assert.False(isValid);
+	}
+
+	[Fact]
+	public void GiveFalseIfHas2ConsecutivePeriodCharacters()
+	{
+		var attribute = new ValidDiscordUsernameAttribute();
+
+		var input = "john..doe";
+		var isValid = attribute.IsValid(input);
+
+		Assert.False(isValid);
+	}
+
+	[Theory]
+	[InlineData("jo")]
+	[InlineData("johndoe")]
+	[InlineData("areallylongjohndoe")]
+	[InlineData("areallylonglonglongjohndoe")]
+	[InlineData("areallylonglonglonglongjohndoe")]
+	public void GiveTrueIfHasAtLeast2CharactersAndAtMost32(string input)
+	{
+		var attribute = new ValidDiscordUsernameAttribute();
+
+		var isValid = attribute.IsValid(input);
+
+		Assert.True(isValid);
+	}
+
+	[Theory]
+	[InlineData(".johndoe")]
+	[InlineData("johndoe123")]
+	[InlineData("john.doe.123")]
+	[InlineData("john_doe.123")]
+	[InlineData("john_doe_123")]
+	public void GiveTrueIfHasNumbersUnderscoreOrPeriod(string input)
+	{
+		var attribute = new ValidDiscordUsernameAttribute();
+
+		var isValid = attribute.IsValid(input);
+
+		Assert.True(isValid);
+	}
+}
