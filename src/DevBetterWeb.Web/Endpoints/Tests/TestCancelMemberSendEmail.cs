4c5f5762367a14aa2c84a47e077993ce0d2ad4ca
ShadyNagy <info@shadynagy.com>
2024-07-29 19:32:43 +0300

    Test endpoint for OutboxServer.

---
+++ src/DevBetterWeb.Web/Endpoints/Tests/TestCancelMemberSendEmail.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using DevBetterWeb.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBetterWeb.Web.Endpoints;
+
+public class TestCancelMemberSendEmail : EndpointBaseAsync
+	.WithoutRequest
+	.WithActionResult
+{
+	private readonly IMemberCancellationService _memberCancellationService;
+
+	public TestCancelMemberSendEmail(IMemberCancellationService memberCancellationService)
+	{
+		_memberCancellationService = memberCancellationService;
+	}
+
+	[HttpGet("tests/member-cancellation-send-email")]
+	public override async Task<ActionResult> HandleAsync(CancellationToken cancellationToken = default)
+	{
+		await _memberCancellationService.SendCancellationEmailAsync("to@example.com");
+
+		return Ok();
+	}
+}
