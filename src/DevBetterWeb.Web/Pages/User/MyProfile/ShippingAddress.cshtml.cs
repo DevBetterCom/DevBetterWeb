eb9551e15bbe937712029222cccdf0e85add0236
ShadyNagy <info@shadynagy.com>
2024-07-30 21:39:43 +0300

    - Admin can change the user address.

--- src/DevBetterWeb.Web/Pages/User/MyProfile/ShippingAddress.cshtml.cs
+++ src/DevBetterWeb.Web/Pages/User/MyProfile/ShippingAddress.cshtml.cs
@@ -11,75 +11,84 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DevBetterWeb.Web.Pages.User
+namespace DevBetterWeb.Web.Pages.User;
+
+[Authorize(Roles = AuthConstants.Roles.ADMINISTRATORS_MEMBERS_ALUMNI)]
+public class ShippingAddressModel : PageModel
 {
-	[Authorize(Roles = AuthConstants.Roles.ADMINISTRATORS_MEMBERS_ALUMNI)]
-	public class ShippingAddressModel : PageModel
+	private readonly UserManager<ApplicationUser> _userManager;
+	private readonly IMemberRegistrationService _memberRegistrationService;
+	private readonly IRepository<Member> _memberRepository;
+
+	public ShippingAddressModel(UserManager<ApplicationUser> userManager,
+		IMemberRegistrationService memberRegistrationService,
+		IRepository<Member> memberRepository)
 	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly IMemberRegistrationService _memberRegistrationService;
-		private readonly IRepository<Member> _memberRepository;
+		_userManager = userManager;
+		_memberRegistrationService = memberRegistrationService;
+		_memberRepository = memberRepository;
+	}
 
-		public ShippingAddressModel(UserManager<ApplicationUser> userManager,
-			IMemberRegistrationService memberRegistrationService,
-			IRepository<Member> memberRepository)
+	[BindProperty]
+	public UserShippingAddressUpdateModel UserShippingAddressUpdateModel { get; set; } = null!;
+
+	public async Task OnGetAsync(string? userId = null)
+	{
+		var (member, actualUserId) = await GetMemberAsync(User, userId);
+		if (member is null && actualUserId is null) return;
+		if (member == null)
 		{
-			_userManager = userManager;
-			_memberRegistrationService = memberRegistrationService;
-			_memberRepository = memberRepository;
+			member = await _memberRegistrationService.RegisterMemberAsync(actualUserId!);
 		}
 
-		[BindProperty] 
-		public UserShippingAddressUpdateModel UserShippingAddressUpdateModel { get; set; } = null!;
+		UserShippingAddressUpdateModel = new UserShippingAddressUpdateModel(member);
+	}
 
-		public async Task OnGetAsync()
+	public async Task OnPost(string? userId = null)
+	{
+		if (!ModelState.IsValid) return;
+
+		var (member, actualUserId) = await GetMemberAsync(User, userId);
+		if (member is null && actualUserId is null) return;
+		if (member is null) throw new MemberNotFoundException(actualUserId!);
+
+		member.UpdateShippingAddress(
+			UserShippingAddressUpdateModel.Street,
+			UserShippingAddressUpdateModel.City,
+			UserShippingAddressUpdateModel.State,
+			UserShippingAddressUpdateModel.PostalCode,
+			UserShippingAddressUpdateModel.Country);
+		member.UpdateAddress(member.ShippingAddress!.ToString());
+
+		await _memberRepository.UpdateAsync(member);
+	}
+
+	private async Task<(Member? member, string? userId)> GetMemberAsync(ClaimsPrincipal user, string? userId = null)
+	{
+		var isAdmin = user.IsInRole(AuthConstants.Roles.ADMINISTRATORS);
+
+		if (isAdmin && !string.IsNullOrEmpty(userId))
 		{
-			var (member, userId) = await GetMemberAsync(User);
-			if (member is null && userId is null) return;
-			if (member == null)
-			{
-				member = await _memberRegistrationService.RegisterMemberAsync(userId!);
-			}
-
-			UserShippingAddressUpdateModel = new UserShippingAddressUpdateModel(member);
-		}
-
-		public async Task OnPost()
-		{
-			if (!ModelState.IsValid) return;
-
-			var (member, userId) = await GetMemberAsync(User);
-			if (member is null && userId is null) return;
-			if (member is null) throw new MemberNotFoundException(userId!);
-
-			member.UpdateShippingAddress(
-				UserShippingAddressUpdateModel.Street, 
-				UserShippingAddressUpdateModel.City,
-				UserShippingAddressUpdateModel.State,
-				UserShippingAddressUpdateModel.PostalCode,
-				UserShippingAddressUpdateModel.Country);
-			member.UpdateAddress(member.ShippingAddress!.ToString());
-
-			await _memberRepository.UpdateAsync(member);
-		}
-
-		private async Task<(Member? member, string? userId)> GetMemberAsync(ClaimsPrincipal user)
-		{
-			var currentUserName = User.Identity?.Name;
-			if (currentUserName == null)
-			{
-				return (null, null);
-			}
-			var applicationUser = await _userManager.FindByNameAsync(currentUserName);
-			if (applicationUser == null)
-			{
-				return (null, null);
-			}
-
-			var spec = new MemberByUserIdSpec(applicationUser.Id);
+			var spec = new MemberByUserIdSpec(userId);
 			var member = await _memberRepository.FirstOrDefaultAsync(spec);
-
-			return (member, applicationUser.Id);
+			return (member, userId);
 		}
+
+		var currentUserName = user.Identity?.Name;
+		if (currentUserName == null)
+		{
+			return (null, null);
+		}
+
+		var applicationUser = await _userManager.FindByNameAsync(currentUserName);
+		if (applicationUser == null)
+		{
+			return (null, null);
+		}
+
+		var userSpec = new MemberByUserIdSpec(applicationUser.Id);
+		var currentUserMember = await _memberRepository.FirstOrDefaultAsync(userSpec);
+
+		return (currentUserMember, applicationUser.Id);
 	}
 }
