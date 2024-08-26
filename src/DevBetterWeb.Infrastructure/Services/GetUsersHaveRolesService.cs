88354b02b2fdd3719cbd840da2068ac24410d998
ShadyNagy <info@shadynagy.com>
2024-08-26 22:47:10 +0300

    - Service to Get users have roles is created.

---
+++ src/DevBetterWeb.Infrastructure/Services/GetUsersHaveRolesService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevBetterWeb.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevBetterWeb.Infrastructure.Services;
+
+public class GetUsersHaveRolesService : IGetUsersHaveRolesService
+{
+	private readonly UserManager<IdentityUser> _userManager;
+
+	public GetUsersHaveRolesService(UserManager<IdentityUser> userManager)
+	{
+		_userManager = userManager;
+	}
+
+	public async Task<List<IdentityUser>> ExecuteAsync()
+	{
+		var usersWithRoles = new List<IdentityUser>();
+
+		var allUsers = _userManager.Users.ToList();
+		foreach (var user in allUsers)
+		{
+			if (user.UserName == null)
+			{
+				continue;
+			}
+			var roles = await _userManager.GetRolesAsync(user);
+
+			if (roles.Any())
+			{
+				usersWithRoles.Add(user);
+			}
+		}
+
+		return usersWithRoles;
+	}
+}
