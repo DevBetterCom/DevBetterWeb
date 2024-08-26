d985be5bb7ca78637830f3e56414bc020638fcb6
ShadyNagy <info@shadynagy.com>
2024-08-26 22:47:59 +0300

    - Send email when video added handler is created.

---
+++ src/DevBetterWeb.Infrastructure/Handlers/SendEmailVideoAddedHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using DevBetterWeb.Core.Events;
+using DevBetterWeb.Core.Interfaces;
+using DevBetterWeb.Infrastructure.DiscordWebooks;
+using DevBetterWeb.Infrastructure.Interfaces;
+
+namespace DevBetterWeb.Core.Handlers;
+
+public class SendEmailVideoAddedHandler : IHandle<VideoAddedEvent>
+{
+	private readonly IGetUsersHaveRolesService _getUsersHaveRolesService;
+	private readonly IEmailService _emailService;
+
+	public SendEmailVideoAddedHandler(IGetUsersHaveRolesService getUsersHaveRolesService, IEmailService emailService)
+	{
+		_getUsersHaveRolesService = getUsersHaveRolesService;
+		_emailService = emailService;
+	}
+
+  public static string ReturnMessageString(VideoAddedEvent domainEvent)
+  {
+    return $"Video {domainEvent.Video.Title} is added! " +
+        $"Check out the video here: https://devbetter.com/Videos/Details/{domainEvent.Video.VideoId}.";
+  }
+
+  public async Task Handle(VideoAddedEvent domainEvent)
+  {
+		var message = ReturnMessageString(domainEvent);
+    var activeUsers = await _getUsersHaveRolesService.ExecuteAsync();
+
+    foreach (var activeUser in activeUsers)
+    {
+			await _emailService.SendEmailAsync(activeUser.UserName!, "DevBetter New Video", message);
+		}
+  }
+}
